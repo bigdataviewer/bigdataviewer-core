df4211ef123c45573505e56de74f34e10e25e8f6
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2012-11-19 19:26:42 +0100

    move loading of registered sequence into Views to separate class.

--- src/viewer/ViewRegisteredAngles.java
+++ src/viewer/ViewRegisteredAngles.java
@@ -2,16 +2,11 @@ package viewer;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Iterator;
 
 import javax.xml.parsers.ParserConfigurationException;
 
 import mpicbg.tracking.data.SequenceDescription;
 import mpicbg.tracking.data.View;
-import mpicbg.tracking.data.ViewRegistration;
-import mpicbg.tracking.data.ViewRegistrations;
 import mpicbg.tracking.transform.AffineModel3D;
 import net.imglib2.ExtendedRandomAccessibleInterval;
 import net.imglib2.RandomAccessibleInterval;
@@ -37,10 +32,6 @@ public class ViewRegisteredAngles
 		new ViewRegisteredAngles( viewRegistrationsFilename );
 	}
 
-	final private SequenceDescription seq;
-
-	final private ArrayList< View > views;
-
 	private Multi3DViewer viewer = null;
 
 	public < T extends NumericType< T > > void show3d( final RandomAccessibleInterval< T > img, final Converter< T, ARGBType > converter, final AffineTransform3D sourceTransform, final String name )
@@ -59,57 +50,17 @@ public class ViewRegisteredAngles
 			viewer.addSource( new Multi3DViewer.SourceAndConverter< T >( source, converter, sourceTransform, name ) );
 	}
 
-	public View getView( final int timepoint, final int setup )
-	{
-		return views.get( timepoint * seq.numViewSetups() + setup );
-	}
-
-	private void createViews( final ViewRegistrations regs )
-	{
-		views.clear();
-
-		if ( seq.numTimepoints() * seq.numViewSetups() != regs.registrations.size() )
-			throw new RuntimeException( "ViewRegistrations does not match SequenceDescription" );
-
-		final ArrayList< ViewRegistration > registrations = new ArrayList< ViewRegistration >( regs.registrations );
-		Collections.sort( registrations, new Comparator< ViewRegistration >()
-		{
-			@Override
-			public int compare( final ViewRegistration o1, final ViewRegistration o2 )
-			{
-				final int dt = o1.getTimepointIndex() - o2.getTimepointIndex();
-				if ( dt == 0 )
-					return o1.getSetupIndex() - o2.getSetupIndex();
-				else
-					return dt;
-			}
-		} );
-
-		final Iterator< ViewRegistration > ri = registrations.iterator();
-		for ( int ti = 0; ti < seq.timepoints.length; ++ti )
-			for ( int si = 0; si < seq.setups.length; ++si )
-			{
-				final ViewRegistration reg = ri.next();
-				if ( reg.getTimepointIndex() == ti && reg.getSetupIndex() == si )
-					views.add( new View( seq, ti, si, reg.getModel() ) );
-				else
-					throw new RuntimeException( "ViewRegistrations does not match SequenceDescription" );
-			}
-	}
-
 	private ViewRegisteredAngles( final String viewRegistrationsFilename ) throws ParserConfigurationException, SAXException, IOException, InstantiationException, IllegalAccessException, ClassNotFoundException
 	{
-		final ViewRegistrations regs = ViewRegistrations.load( viewRegistrationsFilename );
-		seq = SequenceDescription.load( regs.sequenceDescriptionName, true );
-		views = new ArrayList< View >();
-		createViews( regs );
+		final SequenceViewsLoader loader = new SequenceViewsLoader( viewRegistrationsFilename );
+		final SequenceDescription seq = loader.getSequenceDescription();
 
 		final double[][] data = new double[3][4];
 
 		final int timepoint = 0;
 		for ( int setup = 0; setup < seq.numViewSetups(); ++setup )
 		{
-			final View view = getView( timepoint, setup );
+			final View view = loader.getView( timepoint, setup );
 			final AffineModel3D reg = view.getModel();
 			final AffineTransform3D viewTransform = new AffineTransform3D();
 			reg.toMatrix( data );
