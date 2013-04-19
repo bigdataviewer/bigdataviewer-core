9a1860adb077f47e25ce2858dfa9e1ed1fc23e70
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2013-04-19 20:37:24 +0200

    ImagePlus export: added dialog for choosing output path, resolutions, etc.

--- src/main/java/creator/SingleImageTest.java
+++ src/main/java/creator/SingleImageTest.java
@@ -1,5 +1,6 @@
 package creator;
 
+import fiji.util.gui.GenericDialogPlus;
 import ij.IJ;
 import ij.ImagePlus;
 import ij.WindowManager;
@@ -15,13 +16,17 @@ import mpicbg.spim.data.ViewRegistrations;
 import mpicbg.spim.data.ViewSetup;
 import net.imglib2.realtransform.AffineTransform3D;
 import viewer.hdf5.Hdf5ImageLoader;
-import creator.CreateCells.MipMapDefinition;
-import creator.ImagePlusImgLoader.MinMaxOption;
-import creator.spim.WriteSequenceToXml;
+import creator.CreateCells.ProgressListener;
+import creator.ij.ImagePlusImgLoader;
+import creator.ij.ImagePlusImgLoader.MinMaxOption;
 
+/**
+ * ImageJ plugin to export the current image to xml/hdf5.
+ *
+ * @author Tobias Pietzsch <tobias.pietzsch@gmail.com>
+ */
 public class SingleImageTest implements PlugIn
 {
-
 	@Override
 	public void run( final String arg )
 	{
@@ -37,21 +42,42 @@ public class SingleImageTest implements PlugIn
 			return;
 		}
 
+		// check the image type
+		switch ( imp.getType() )
+		{
+		case ImagePlus.GRAY8:
+		case ImagePlus.GRAY16:
+		case ImagePlus.GRAY32:
+			break;
+		default:
+			IJ.showMessage( "Only 8, 16, 32-bit images are supported currently!" );
+			return;
+		}
+
+		// check the image dimensionality
+		if ( imp.getNDimensions() < 3 )
+		{
+			IJ.showMessage( "Image must be at least 3-dimensional!" );
+			return;
+		}
+
+		// show dialog to get output paths, resolutions, subdivisions, min-max option
+		final Parameters params = getParameters( imp.getDisplayRangeMin(), imp.getDisplayRangeMax() );
+
 		// create ImgLoader wrapping the image
 		final ImgLoader imgLoader;
-		switch( imp.getType() )
+		switch ( imp.getType() )
 		{
-			case ImagePlus.GRAY8 :
-				imgLoader = ImagePlusImgLoader.createGray8( imp, MinMaxOption.TAKE_FROM_IMAGEPROCESSOR, 0, 0 );
-				break;
-			case ImagePlus.GRAY16 :
-				imgLoader = ImagePlusImgLoader.createGray16( imp, MinMaxOption.TAKE_FROM_IMAGEPROCESSOR, 0, 0 );
-				break;
-			case ImagePlus.GRAY32 :
-				imgLoader = ImagePlusImgLoader.createGray32( imp, MinMaxOption.TAKE_FROM_IMAGEPROCESSOR, 0, 0 );
-				break;
-			default :
-				throw new RuntimeException("Only 8, 16, 32-bit images supported!");
+		case ImagePlus.GRAY8:
+			imgLoader = ImagePlusImgLoader.createGray8( imp, params.minMaxOption, params.rangeMin, params.rangeMax );
+			break;
+		case ImagePlus.GRAY16:
+			imgLoader = ImagePlusImgLoader.createGray16( imp, params.minMaxOption, params.rangeMin, params.rangeMax );
+			break;
+		case ImagePlus.GRAY32:
+		default:
+			imgLoader = ImagePlusImgLoader.createGray32( imp, params.minMaxOption, params.rangeMin, params.rangeMax );
+			break;
 		}
 
 		final double pw = imp.getCalibration().pixelWidth;
@@ -63,17 +89,22 @@ public class SingleImageTest implements PlugIn
 
 		// create SourceTransform from the images calibration
 		final AffineTransform3D sourceTransform = new AffineTransform3D();
-		sourceTransform.set(
-				pw, 0, 0, 0,
-				0, ph, 0, 0,
-				0, 0, pd, 0 );
+		sourceTransform.set( pw, 0, 0, 0, 0, ph, 0, 0, 0, 0, pd, 0 );
 
 		// write hdf5
-		final File seqFile = new File( "/Users/tobias/Desktop/l1-reconstructed.xml" );
-		final File hdf5File = new File( "/Users/tobias/Desktop/l1-reconstructed.h5" );
-		final int[][] resolutions = MipMapDefinition.resolutions;
-		final int[][] subdivisions = MipMapDefinition.subdivisions;
-		CreateCells.createHdf5File( 1, 1, imgLoader, resolutions, subdivisions, hdf5File, null );
+		final File seqFile = params.seqFile;
+		final File hdf5File = params.hdf5File;
+		final int[][] resolutions = params.resolutions;
+		final int[][] subdivisions = params.subdivisions;
+		final ProgressListener progressListener = new ProgressListener()
+		{
+			@Override
+			public void updateProgress( final int numCompletedTasks, final int numTasks )
+			{
+				IJ.showProgress( numCompletedTasks, numTasks + 1 );
+			}
+		};
+		CreateCells.createHdf5File( 1, 1, imgLoader, resolutions, subdivisions, hdf5File, progressListener );
 
 		// write xml sequence description
 		final ViewSetup[] setups = new ViewSetup[] { new ViewSetup( 0, 0, 0, 0, w, h, d, pw, ph, pd ) };
@@ -86,17 +117,135 @@ public class SingleImageTest implements PlugIn
 		try
 		{
 			WriteSequenceToXml.writeSequenceToXml( sequenceDescription, viewRegistrations, seqFile.getAbsolutePath() );
+			IJ.showProgress( 1 );
 		}
 		catch ( final Exception e )
 		{
 			throw new RuntimeException( e );
 		}
 
-		// TODO: progress listener
-
-		// TODO: create dialog to query output paths, resolutions, subdivisions, min-max option, name
-
 		IJ.log( "SpimViewer import done." );
 	}
 
+	protected static class Parameters
+	{
+		final int[][] resolutions;
+
+		final int[][] subdivisions;
+
+		final File seqFile;
+
+		final File hdf5File;
+
+		final MinMaxOption minMaxOption;
+
+		final double rangeMin;
+
+		final double rangeMax;
+
+		public Parameters( final int[][] resolutions, final int[][] subdivisions, final File seqFile, final File hdf5File, final MinMaxOption minMaxOption, final double rangeMin, final double rangeMax )
+		{
+			this.resolutions = resolutions;
+			this.subdivisions = subdivisions;
+			this.seqFile = seqFile;
+			this.hdf5File = hdf5File;
+			this.minMaxOption = minMaxOption;
+			this.rangeMin = rangeMin;
+			this.rangeMax = rangeMax;
+		}
+	}
+
+	static String lastSubsampling = "{1,1,1}, {2,2,1}, {4,4,2}";
+
+	static String lastChunkSizes = "{32,32,4}, {16,16,8}, {8,8,8}";
+
+	static int lastMinMaxChoice = 0;
+
+	static double lastMin = 0;
+
+	static double lastMax = 0;
+
+	static String lastExportPath = "./export.xml";
+
+	protected Parameters getParameters( final double impMin, final double impMax )
+	{
+		if ( lastMinMaxChoice == 0 ) // use ImageJs...
+		{
+			lastMin = impMin;
+			lastMax = impMax;
+		}
+
+		while ( true )
+		{
+			final GenericDialogPlus gd = new GenericDialogPlus( "SpimViewer Import" );
+
+			gd.addStringField( "Subsampling factors", lastSubsampling, 25 );
+			gd.addStringField( "Hdf5 chunk sizes", lastChunkSizes, 25 );
+
+			gd.addMessage( "" );
+			final String[] minMaxChoices = new String[] { "Use ImageJ's current min/max setting", "Compute min/max of the (hyper-)stack", "Use values specified below" };
+			gd.addChoice( "Value range", minMaxChoices, minMaxChoices[ lastMinMaxChoice ] );
+			gd.addNumericField( "Min", lastMin, 0 );
+			gd.addNumericField( "Max", lastMax, 0 );
+			gd.addMessage( "" );
+			PluginHelper.addSaveAsFileField( gd, "Export path", lastExportPath, 25 );
+
+//			gd.addMessage( "" );
+//			gd.addMessage( "This Plugin is developed by Tobias Pietzsch (pietzsch@mpi-cbg.de)\n" );
+//			Bead_Registration.addHyperLinkListener( ( MultiLineLabel ) gd.getMessage(), "mailto:pietzsch@mpi-cbg.de" );
+
+			gd.showDialog();
+			if ( gd.wasCanceled() )
+				return null;
+
+			lastSubsampling = gd.getNextString();
+			lastChunkSizes = gd.getNextString();
+			lastMinMaxChoice = gd.getNextChoiceIndex();
+			lastMin = gd.getNextNumber();
+			lastMax = gd.getNextNumber();
+			lastExportPath = gd.getNextString();
+
+			// parse mipmap resolutions and cell sizes
+			final int[][] resolutions = PluginHelper.parseResolutionsString( lastSubsampling );
+			final int[][] subdivisions = PluginHelper.parseResolutionsString( lastChunkSizes );
+			if ( resolutions.length == 0 )
+			{
+				IJ.showMessage( "Cannot parse subsampling factors " + lastSubsampling );
+				continue;
+			}
+			if ( subdivisions.length == 0 )
+			{
+				IJ.showMessage( "Cannot parse hdf5 chunk sizes " + lastChunkSizes );
+				continue;
+			}
+			else if ( resolutions.length != subdivisions.length )
+			{
+				IJ.showMessage( "subsampling factors and hdf5 chunk sizes must have the same number of elements" );
+				continue;
+			}
+
+			final MinMaxOption minMaxOption;
+			if ( lastMinMaxChoice == 0 )
+				minMaxOption = MinMaxOption.TAKE_FROM_IMAGEPROCESSOR;
+			else if ( lastMinMaxChoice == 1 )
+				minMaxOption = MinMaxOption.COMPUTE;
+			else
+				minMaxOption = MinMaxOption.SET;
+
+			String seqFilename = lastExportPath;
+			if ( !seqFilename.endsWith( ".xml" ) )
+				seqFilename += ".xml";
+			final File seqFile = new File( seqFilename );
+			final File parent = seqFile.getParentFile();
+			if ( parent == null || !parent.exists() || !parent.isDirectory() )
+			{
+				IJ.showMessage( "Invalid export filename " + seqFilename );
+				continue;
+			}
+			final String hdf5Filename = seqFilename.substring( 0, seqFilename.length() - 4 ) + ".h5";
+			final File hdf5File = new File( hdf5Filename );
+
+			return new Parameters( resolutions, subdivisions, seqFile, hdf5File, minMaxOption, lastMin, lastMax );
+		}
+	}
 }
