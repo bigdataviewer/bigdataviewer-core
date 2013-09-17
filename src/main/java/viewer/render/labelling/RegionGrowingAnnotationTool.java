d0c2fdc177c6b47ccc8d8e83ea2a445f14c2e54d
Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>
2013-09-17 10:20:50 +0200

    Make the region growing tool almost work
    Signed-off-by: Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>

--- src/main/java/viewer/render/labelling/RegionGrowingAnnotationTool.java
+++ src/main/java/viewer/render/labelling/RegionGrowingAnnotationTool.java
@@ -5,6 +5,7 @@ import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import net.imglib2.IterableInterval;
@@ -15,13 +16,11 @@ import net.imglib2.RealPoint;
 import net.imglib2.algorithm.regiongrowing.RegionGrowingTools.GrowingMode;
 import net.imglib2.algorithm.regiongrowing.ThresholdRegionGrowing;
 import net.imglib2.labeling.LabelingType;
-import net.imglib2.labeling.NativeImgLabeling;
 import net.imglib2.ops.operation.randomaccessibleinterval.unary.regiongrowing.AbstractRegionGrowing;
 import net.imglib2.position.transform.Round;
 import net.imglib2.realtransform.AffineTransform3D;
 import net.imglib2.roi.IterableRegionOfInterest;
 import net.imglib2.type.numeric.RealType;
-import net.imglib2.type.numeric.integer.IntType;
 import viewer.SpimViewer;
 import viewer.render.Source;
 
@@ -34,6 +33,8 @@ public class RegionGrowingAnnotationTool<T extends RealType<T>> implements Mouse
 	private int label = -1;
 	private final Source<T> source;
 	private final int t;
+	private final long[][] structuringElement;
+	private final GrowingMode growingMode;
 
 	public RegionGrowingAnnotationTool(final SpimViewer viewer, final Source<T> source, final LabellingSource labellingSource, final int t, final int level) {
 		this.viewer = viewer;
@@ -41,40 +42,63 @@ public class RegionGrowingAnnotationTool<T extends RealType<T>> implements Mouse
 		this.labellingSource = labellingSource;
 		this.t = t;
 		this.level = level;
+		this.structuringElement = AbstractRegionGrowing.get8ConStructuringElement(3);
+		this.growingMode = GrowingMode.SEQUENTIAL;
 	}
 
 	@Override
-	public void mouseClicked(final MouseEvent e) {
-		// TODO Auto-generated method stub
-
-	}
+	public void mouseClicked(final MouseEvent e) {}
 
 	@Override
-	public void mouseEntered(final MouseEvent e) {
-		// TODO Auto-generated method stub
-
-	}
+	public void mouseEntered(final MouseEvent e) {}
 
 	@Override
-	public void mouseExited(final MouseEvent e) {
-		// TODO Auto-generated method stub
-
-	}
+	public void mouseExited(final MouseEvent e) {}
 
 	@Override
 	public void mousePressed(final MouseEvent e) {
-		label++;
 		final Point point = getLocationOnCurrentSource();
 		final long[] pos = new long[3];
 		point.localize(pos);
+
+		if (!labellingSource.getCurrentLabelling().getLabels().isEmpty()) {
+
+			final RandomAccess<LabelingType<Integer>> randomAccess = labellingSource.getCurrentLabelling().randomAccess();
+			randomAccess.setPosition(point);
+
+			final List<Integer> existingLabels = randomAccess.get().getLabeling();
+			if (!existingLabels.isEmpty()) {
+
+				label = existingLabels.get(0); // For future new annotation
+
+				for (final Integer existingLabel : existingLabels) {
+
+					final IterableRegionOfInterest roi = labellingSource.getCurrentLabelling().getIterableRegionOfInterest(existingLabel);
+					final IterableInterval<LabelingType<Integer>> overROI = roi.getIterableIntervalOverROI(labellingSource.getCurrentLabelling());
+					for (final LabelingType<Integer> labelingType : overROI) {
+						final ArrayList<Integer> labels = new ArrayList<Integer>(labelingType.getLabeling());
+						labels.clear();
+						labelingType.setLabeling(labels);
+					}
+
+				}
+			} else {
+
+				label++;
+			}
+
+		} else {
+			label++;
+		}
+
 		seed = new HashMap<long[], Integer>(1);
 		seed.put(pos, Integer.valueOf(label));
 	}
 
 	@Override
 	public void mouseReleased(final MouseEvent e) {
-		// TODO Auto-generated method stub
-
+		labellingSource.updateColorTable();
+		viewer.requestRepaint();
 	}
 
 	@Override
@@ -84,46 +108,22 @@ public class RegionGrowingAnnotationTool<T extends RealType<T>> implements Mouse
 		 * Remove old stuff
 		 */
 
-		if (labellingSource.getCurrentLabelling().getLabels().contains(Integer.valueOf(label))) {
-			final IterableRegionOfInterest roi = labellingSource.getCurrentLabelling().getIterableRegionOfInterest(Integer.valueOf(label));
-			final IterableInterval<LabelingType<Integer>> overROI = roi.getIterableIntervalOverROI(labellingSource.getCurrentLabelling());
-			for (final LabelingType<Integer> labelingType : overROI) {
-				//				labelingType.getLabeling().remove(Integer.valueOf(label));
-				final ArrayList<Integer> labels = new ArrayList<Integer>(labelingType.getLabeling());
-				labels.remove(Integer.valueOf(label));
-				labelingType.setLabeling(labels);
-			}
-		}
-
 		final RandomAccessibleInterval<T> rai = source.getSource(t, level);
 		final RandomAccess<T> ra = rai.randomAccess();
 
 		final Point current = getLocationOnCurrentSource();
 		ra.setPosition(current);
-		final T threshold = ra.get();
+		final T threshold = ra.get().copy();
 
-		final GrowingMode growingMode = GrowingMode.SEQUENTIAL;
-		final long[][] structuringElement = AbstractRegionGrowing.get8ConStructuringElement(3);
-
-		final ThresholdRegionGrowing<T, Integer> regionGrowing = new ThresholdRegionGrowing<T, Integer>(rai, threshold, seed, growingMode, structuringElement) {
-
-			@Override
-			public NativeImgLabeling<Integer, IntType> initializeLabeling() {
-				return labellingSource.getCurrentLabelling();
-			}
-		};
+		final ThresholdRegionGrowing<T, Integer> regionGrowing = new ThresholdRegionGrowing<T, Integer>(ra, threshold, seed, growingMode, structuringElement, labellingSource.getCurrentLabelling());
 
 		regionGrowing.checkInput();
 		regionGrowing.process();
-		System.out.println(regionGrowing.getResult());// DEBUG
-		System.out.println(regionGrowing.getResult().getArea(Integer.valueOf(label)));// DEBUG
+		System.out.println(label + ": area = " + regionGrowing.getResult().getArea(Integer.valueOf(label)));// DEBUG
 	}
 
 	@Override
-	public void mouseMoved(final MouseEvent arg0) {
-		// TODO Auto-generated method stub
-
-	}
+	public void mouseMoved(final MouseEvent arg0) {}
 
 	private Point getLocationOnCurrentSource() {
 
