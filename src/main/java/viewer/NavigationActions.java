d8c7e9d50712f81fd015c40381c331c39ca8e0ed
Jean-Yves Tinevez <jeanyves.tinevez@gmail.com>
2013-09-18 22:25:52 +0200

    Externalize keystrokes handling
    Keystroks and bound actions are handled in a separate
    class, that can load a key mapping from a file. The goal
    is to have keybindings that can be toggled on/off depending
    on the mode we are in.
    
    Signed-off-by: Jean-Yves Tinevez <jeanyves.tinevez@gmail.com>
    
    Conflicts:
    	src/main/java/viewer/SpimViewer.java

---
+++ src/main/java/viewer/NavigationActions.java
@@ -0,0 +1,142 @@
+package viewer;
+
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+
+import viewer.SpimViewer.AlignPlane;
+
+public class NavigationActions
+{
+
+	private NavigationActions()
+	{}
+
+	public static final Action getToggleManualTransformationAction( final SpimViewer viewer )
+	{
+		return new AbstractAction( "toggle manual transformation" )
+		{
+			@Override
+			public void actionPerformed( final ActionEvent e )
+			{
+				viewer.toggleManualTransformation();
+			}
+
+			private static final long serialVersionUID = 1L;
+		};
+	}
+
+	public static final Action getToggleInterpolationAction( final SpimViewer viewer )
+	{
+		return new AbstractAction( "toggle interpolation" )
+		{
+			@Override
+			public void actionPerformed( final ActionEvent e )
+			{
+				viewer.toggleInterpolation();
+			}
+
+			private static final long serialVersionUID = 1L;
+		};
+	}
+
+	public static final Action getToggleFusedModeAction( final SpimViewer viewer )
+	{
+		return new AbstractAction( "toggle fused mode" )
+		{
+			@Override
+			public void actionPerformed( final ActionEvent e )
+			{
+				viewer.visibilityAndGrouping.setFusedEnabled( !viewer.visibilityAndGrouping.isFusedEnabled() );
+			}
+
+			private static final long serialVersionUID = 1L;
+		};
+
+	}
+
+	public static final Action getToggleGroupingAction( final SpimViewer viewer )
+	{
+		return new AbstractAction( "toggle grouping" )
+		{
+			@Override
+			public void actionPerformed( final ActionEvent e )
+			{
+				viewer.visibilityAndGrouping.setGroupingEnabled( !viewer.visibilityAndGrouping.isGroupingEnabled() );
+			}
+
+			private static final long serialVersionUID = 1L;
+		};
+	}
+
+	public static final Action getSetCurrentSource( final SpimViewer viewer, final int sourceIndex )
+	{
+		return new AbstractAction( "set current source " + sourceIndex )
+		{
+			@Override
+			public void actionPerformed( final ActionEvent e )
+			{
+				viewer.setCurrentGroupOrSource( sourceIndex );
+			}
+
+			private static final long serialVersionUID = 1L;
+		};
+	}
+
+	public static final Action getToggleSourceVisibilityAction( final SpimViewer viewer, final int sourceIndex )
+	{
+		return new AbstractAction( "toggle source visibility " + sourceIndex )
+		{
+			@Override
+			public void actionPerformed( final ActionEvent e )
+			{
+				viewer.toggleActiveGroupOrSource( sourceIndex );
+			}
+
+			private static final long serialVersionUID = 1L;
+		};
+	}
+
+	public static final Action getAlignPlaneAction( final SpimViewer viewer, final AlignPlane plane )
+	{
+		return new AbstractAction( "align XY plane" )
+		{
+			@Override
+			public void actionPerformed( final ActionEvent e )
+			{
+				viewer.align( plane );
+			}
+
+			private static final long serialVersionUID = 1L;
+		};
+	}
+
+	public static final Action getNextTimePointAction( final SpimViewer viewer )
+	{
+		return new AbstractAction( "next timepoint" )
+		{
+			@Override
+			public void actionPerformed( final ActionEvent e )
+			{
+				viewer.sliderTime.setValue( viewer.sliderTime.getValue() + 1 );
+			}
+
+			private static final long serialVersionUID = 1L;
+		};
+	}
+
+	public static Action getPreviousTimePointAction( final SpimViewer viewer )
+	{
+		return new AbstractAction( "next timepoint" )
+		{
+			@Override
+			public void actionPerformed( final ActionEvent e )
+			{
+				viewer.sliderTime.setValue( viewer.sliderTime.getValue() - 1 );
+			}
+
+			private static final long serialVersionUID = 1L;
+		};
+	}
+}
---
+++ src/main/java/viewer/NavigationKeyHandler.java
@@ -0,0 +1,128 @@
+package viewer;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import javax.swing.ActionMap;
+import javax.swing.InputMap;
+import javax.swing.JComponent;
+import javax.swing.KeyStroke;
+import javax.swing.SwingUtilities;
+
+import viewer.SpimViewer.AlignPlane;
+
+public class NavigationKeyHandler
+{
+
+	private static final Properties DEFAULT_KEYBINGS = new Properties();
+	static
+	{
+		DEFAULT_KEYBINGS.setProperty( "T", "toggle manual transformation" );
+		DEFAULT_KEYBINGS.setProperty( "I", "toggle interpolation" );
+		DEFAULT_KEYBINGS.setProperty( "F", "toggle fused mode" );
+		DEFAULT_KEYBINGS.setProperty( "G", "toggle grouping" );
+
+		final String[] numkeys = new String[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "0" };
+		for ( int i = 0; i < numkeys.length; ++i )
+		{
+			DEFAULT_KEYBINGS.setProperty( numkeys[ i ], "set current source " + i );
+			DEFAULT_KEYBINGS.setProperty( "shift " + numkeys[ i ], "toggle source visibility " + i );
+		}
+
+		DEFAULT_KEYBINGS.setProperty( "shift Z", "align XY plane" );
+		DEFAULT_KEYBINGS.setProperty( "shift X", "align ZY plane" );
+		DEFAULT_KEYBINGS.setProperty( "shift Y", "align XZ plane" );
+		DEFAULT_KEYBINGS.setProperty( "shift A", "align XZ plane" );
+
+		DEFAULT_KEYBINGS.setProperty( "CLOSE_BRACKET", "next timepoint" );
+		DEFAULT_KEYBINGS.setProperty( "M", "next timepoint" );
+		DEFAULT_KEYBINGS.setProperty( "OPEN_BRACKET", "previous timepoint" );
+		DEFAULT_KEYBINGS.setProperty( "N", "previous timepoint" );
+	}
+
+	private final SpimViewer viewer;
+
+	public NavigationKeyHandler( final SpimViewer viewer )
+	{
+		this.viewer = viewer;
+
+		installKeyboardActions( viewer.frame.getRootPane() );
+	}
+
+	/**
+	 * Unused, yet.
+	 * 
+	 * @return
+	 */
+	protected InputMap readPropertyFile()
+	{
+		Properties config;
+		final File file = new File( "spimviewer.properties" );
+		try
+		{
+			final InputStream stream = new FileInputStream( file );
+			config = new Properties();
+			config.load( stream );
+		}
+		catch ( final IOException e )
+		{
+			System.out.println( "Cannot find the config file :" + file + ". Using default key bindings." );
+			System.out.println( e.getMessage() );
+			config = DEFAULT_KEYBINGS;
+		}
+
+		return generateMapFrom( config );
+	}
+
+	private InputMap generateMapFrom( final Properties config )
+	{
+		final InputMap map = new InputMap();
+		for ( final Object obj : config.keySet() )
+		{
+
+			final String key = ( String ) obj;
+			final String command = config.getProperty( key );
+			map.put( KeyStroke.getKeyStroke( key ), command );
+		}
+
+		return map;
+	}
+
+	protected void installKeyboardActions( final JComponent component )
+	{
+		final InputMap inputMap = readPropertyFile();
+
+		SwingUtilities.replaceUIActionMap( component, createActionMap() );
+		SwingUtilities.replaceUIInputMap( component, JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT, inputMap );
+		// SwingUtilities.replaceUIInputMap( component, JComponent.WHEN_FOCUSED,
+		// inputMap );
+	}
+
+	protected ActionMap createActionMap()
+	{
+		final ActionMap map = new ActionMap();
+
+		map.put( "toggle manual transformation", NavigationActions.getToggleManualTransformationAction( viewer ) );
+		map.put( "toggle interpolation", NavigationActions.getToggleInterpolationAction( viewer ) );
+		map.put( "toggle fused mode", NavigationActions.getToggleFusedModeAction( viewer ) );
+		map.put( "toggle grouping", NavigationActions.getToggleGroupingAction( viewer ) );
+
+		for ( int i = 0; i < 10; ++i )
+		{
+			map.put( "set current source " + i, NavigationActions.getSetCurrentSource( viewer, i ) );
+			map.put( "toggle source visibility " + i, NavigationActions.getToggleSourceVisibilityAction( viewer, i ) );
+		}
+
+		map.put( "align XY plane", NavigationActions.getAlignPlaneAction( viewer, AlignPlane.XY ) );
+		map.put( "align ZY plane", NavigationActions.getAlignPlaneAction( viewer, AlignPlane.ZY ) );
+		map.put( "align XZ plane", NavigationActions.getAlignPlaneAction( viewer, AlignPlane.XZ ) );
+
+		map.put( "next timepoint", NavigationActions.getNextTimePointAction( viewer ) );
+		map.put( "previous timepoint", NavigationActions.getPreviousTimePointAction( viewer ) );
+
+		return map;
+	}
+}
