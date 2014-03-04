b0d678e4b1ee6507f3d1b7716fdf7ee29465f651
Stephan Saalfeld <saalfelds@janelia.hhmi.org>
2014-03-04 11:33:12 -0500

    added VolatileUnsignedByteType... more to come I guess...

---
+++ src/main/java/net/imglib2/img/basictypeaccess/volatiles/VolatileByteAccess.java
@@ -0,0 +1,11 @@
+package net.imglib2.img.basictypeaccess.volatiles;
+
+import net.imglib2.img.basictypeaccess.ByteAccess;
+
+
+/**
+ * @author Stephan Saalfeld <saalfelds@janelia.hhmi.org>
+ * @author Tobias Pietzsch <tobias.pietzsch@gmail.com>
+ */
+public interface VolatileByteAccess extends ByteAccess, VolatileAccess
+{}
---
+++ src/main/java/net/imglib2/img/basictypeaccess/volatiles/array/VolatileByteArray.java
@@ -0,0 +1,52 @@
+package net.imglib2.img.basictypeaccess.volatiles.array;
+
+import net.imglib2.img.basictypeaccess.volatiles.VolatileByteAccess;
+
+/**
+ * A {@link ByteArray} with an {@link #isValid()} flag.
+ *
+ * @author Stephan Saalfeld <saalfelds@janelia.hhmi.org>
+ * @author Tobias Pietzsch <tobias.pietzsch@gmail.com>
+ */
+public class VolatileByteArray extends AbstractVolatileArray< VolatileByteArray > implements VolatileByteAccess
+{
+	private static final long serialVersionUID = -2609245209721069962L;
+
+	protected byte[] data;
+
+	public VolatileByteArray( final int numEntities, final boolean isValid )
+	{
+		super( isValid );
+		this.data = new byte[ numEntities ];
+	}
+
+	public VolatileByteArray( final byte[] data, final boolean isValid )
+	{
+		super( isValid );
+		this.data = data;
+	}
+
+	@Override
+	public byte getValue( final int index )
+	{
+		return data[ index ];
+	}
+
+	@Override
+	public void setValue( final int index, final byte value )
+	{
+		data[ index ] = value;
+	}
+
+	@Override
+	public VolatileByteArray createArray( final int numEntities )
+	{
+		return new VolatileByteArray( numEntities, true );
+	}
+
+	@Override
+	public byte[] getCurrentStorageArray()
+	{
+		return data;
+	}
+}
