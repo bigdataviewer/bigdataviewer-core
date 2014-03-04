b98b55b57eabc88caadbc57b1c9b9d18104db1ee
Stephan Saalfeld <saalfelds@janelia.hhmi.org>
2014-03-04 10:31:11 -0500

    read urlFormat instead of baseUrl from CATMAID xml description

--- src/main/java/bdv/img/catmaid/CatmaidImageLoader.java
+++ src/main/java/bdv/img/catmaid/CatmaidImageLoader.java
@@ -34,7 +34,7 @@ public class CatmaidImageLoader implements ViewerImgLoader< ARGBType, VolatileAR
 
 	private double resZ;
 
-	private String baseUrl;
+	private String urlFormat;
 
 	private int tileWidth;
 
@@ -60,8 +60,8 @@ public class CatmaidImageLoader implements ViewerImgLoader< ARGBType, VolatileAR
 		resXY = Double.parseDouble( elem.getChildText( "resXY" ) );
 		resZ = Double.parseDouble( elem.getChildText( "resZ" ) );
 
-		baseUrl = elem.getChildText( "baseUrl" );
-
+		urlFormat = elem.getChildText( "urlFormat" );
+		
 		tileWidth = Integer.parseInt( elem.getChildText( "tileWidth" ) );
 		tileHeight = Integer.parseInt( elem.getChildText( "tileHeight" ) );
 
@@ -84,7 +84,7 @@ public class CatmaidImageLoader implements ViewerImgLoader< ARGBType, VolatileAR
 
 		final int[] maxLevels = new int[] { numScales - 1 };
 		cache = new VolatileGlobalCellCache< VolatileIntArray >(
-				new CatmaidVolatileIntArrayLoader( baseUrl + "%5$d/%8$d_%9$d_%1$d.jpg", tileWidth, tileHeight ), 1, 1, numScales, maxLevels, 10 );
+				new CatmaidVolatileIntArrayLoader( urlFormat, tileWidth, tileHeight ), 1, 1, numScales, maxLevels, 10 );
 	}
 
 	final static public int getNumScales( long width, long height, final long tileWidth, final long tileHeight )
