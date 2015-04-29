f0e7bd4adb8fdf1d37d6f713c8733c7cde242a6a
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2015-04-30 01:58:00 +0200

    basic volume rendering with blocks works

--- src/main/java/bdv/cl/RenderSlice.java
+++ src/main/java/bdv/cl/RenderSlice.java
@@ -1,35 +1,49 @@
 package bdv.cl;
 
+import java.nio.ByteBuffer;
+import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
 import java.util.ArrayList;
 
 import mpicbg.spim.data.sequence.ViewId;
 import net.imglib2.Cursor;
 import net.imglib2.Dimensions;
-import net.imglib2.Localizable;
 import net.imglib2.RealPoint;
 import net.imglib2.algorithm.kdtree.HyperPlane;
 import net.imglib2.img.Img;
 import net.imglib2.img.array.ArrayImgs;
 import net.imglib2.img.basictypeaccess.volatiles.array.VolatileShortArray;
+import net.imglib2.img.display.imagej.ImageJFunctions;
 import net.imglib2.iterator.IntervalIterator;
 import net.imglib2.realtransform.AffineTransform3D;
 import net.imglib2.type.numeric.integer.UnsignedShortType;
 import net.imglib2.util.IntervalIndexer;
+import net.imglib2.util.Intervals;
 import net.imglib2.util.LinAlgHelpers;
 import net.imglib2.util.Util;
 import net.imglib2.view.Views;
-import bdv.VolatileSpimSource;
+import bdv.cl.BlockTexture.BlockKey;
 import bdv.img.cache.CacheHints;
 import bdv.img.cache.CachedCellImg;
 import bdv.img.cache.LoadingStrategy;
 import bdv.img.cache.VolatileCell;
 import bdv.img.cache.VolatileGlobalCellCache;
 import bdv.img.hdf5.Hdf5ImageLoader;
+import bdv.viewer.Source;
 import bdv.viewer.state.ViewerState;
 
+import com.jogamp.common.nio.Buffers;
+import com.jogamp.opencl.CLBuffer;
 import com.jogamp.opencl.CLCommandQueue;
 import com.jogamp.opencl.CLContext;
 import com.jogamp.opencl.CLDevice;
+import com.jogamp.opencl.CLImage2d;
+import com.jogamp.opencl.CLImageFormat;
+import com.jogamp.opencl.CLImageFormat.ChannelOrder;
+import com.jogamp.opencl.CLImageFormat.ChannelType;
+import com.jogamp.opencl.CLKernel;
+import com.jogamp.opencl.CLMemory.Map;
+import com.jogamp.opencl.CLMemory.Mem;
 import com.jogamp.opencl.CLProgram;
 
 public class RenderSlice
@@ -38,7 +52,15 @@ public class RenderSlice
 
 	private CLContext cl;
 
-	private CLCommandQueue q;
+	private CLCommandQueue queue;
+
+	private BlockTexture blockTexture;
+
+	private CLKernel slice4;
+
+	private CLBuffer< FloatBuffer > transformMatrix;
+
+	private CLBuffer< IntBuffer > sizes;
 
 	public RenderSlice( final Hdf5ImageLoader imgLoader )
 	{
@@ -52,14 +74,19 @@ public class RenderSlice
 			for ( final CLDevice dev : cl.getDevices() )
 			{
 				if ( "GeForce GT 650M".equals( dev.getName() ) )
+//				if ( "HD Graphics 4000".equals( dev.getName() ) )
 				{
 					device = dev;
 					break;
 				}
 			}
-			q = device.createCommandQueue();
+			queue = device.createCommandQueue();
 
 			final CLProgram program = cl.createProgram( this.getClass().getResourceAsStream( "slice.cl" ) ).build();
+			slice4 = program.createCLKernel( "slice4" );
+
+			transformMatrix = cl.createFloatBuffer( 12, Mem.READ_ONLY, Mem.ALLOCATE_BUFFER );
+			sizes = cl.createIntBuffer( 8, Mem.READ_ONLY, Mem.ALLOCATE_BUFFER );
 		}
 		catch ( final Exception e )
 		{
@@ -70,10 +97,16 @@ public class RenderSlice
 
 	public void cleanUp()
 	{
-		if ( q != null )
-			q.release();
+		if ( queue != null && ! queue.isReleased() )
+			queue.release();
 
-		if ( cl != null )
+		if ( transformMatrix != null && ! transformMatrix.isReleased() )
+			transformMatrix.release();
+
+		if ( sizes != null && ! sizes.isReleased() )
+			sizes.release();
+
+		if ( cl != null && ! cl.isReleased() )
 			cl.release();
 	}
 
@@ -81,10 +114,10 @@ public class RenderSlice
 	{
 		System.out.println( "render slice" );
 
-		final VolatileSpimSource< ?, ? > source = ( VolatileSpimSource< ?, ? > ) viewerState.getSources().get( 0 ).asVolatile().getSpimSource(); // TODO
+		final Source< ? > source = viewerState.getSources().get( 0 ).getSpimSource(); // TODO
 		final int timepoint = 0; // TODO
 		final int timepointId = 0; // TODO
-		final int setupId = source.getSetupId(); // TODO
+		final int setupId = 0; // TODO
 		final int mipmapIndex = 0; // TODO
 
 		final AffineTransform3D sourceToScreen = new AffineTransform3D();
@@ -95,31 +128,95 @@ public class RenderSlice
 
 		final int[] blockSize = imgLoader.getMipmapInfo( setupId ).getSubdivisions()[ 0 ];
 
-		getRequiredBlocks( sourceToScreen, width, height, 10, new ViewId( timepointId, setupId ) );
+		final RequiredBlocks requiredBlocks = getRequiredBlocks( sourceToScreen, width, height, 100, new ViewId( timepointId, setupId ) );
+
+		blockTexture = new BlockTexture( new int[] { 10, 10, 10 }, blockSize, queue );
+		for ( final int[] cellPos : requiredBlocks.cellPositions )
+		{
+			final BlockKey key = new BlockKey( cellPos );
+			if ( ! blockTexture.contains( key ) )
+				blockTexture.put( key, getCellData( cellPos ) );
+		}
+
+		final int[] lookupDims = new int[ 3 ];
+		final int[] maxCell = requiredBlocks.maxCell;
+		final int[] minCell = requiredBlocks.minCell;
+		for ( int d = 0; d < 3; ++d )
+			lookupDims[ d ] = maxCell[ d ] - minCell[ d ] + 1;
+
+		final CLBuffer< IntBuffer > blockLookup = cl.createIntBuffer( 4 * ( int ) Intervals.numElements( lookupDims ), Mem.READ_ONLY, Mem.ALLOCATE_BUFFER );
+
+		final ByteBuffer bytes = queue.putMapBuffer( blockLookup, Map.WRITE, true );
+		final IntBuffer ints = bytes.asIntBuffer();
+		for ( final int[] cellPos : requiredBlocks.cellPositions )
+		{
+			final BlockKey key = new BlockKey( cellPos );
+			final int[] blockPos = blockTexture.get( key ).getBlockPos();
+			final int i = 4 * IntervalIndexer.positionWithOffsetToIndex( cellPos, lookupDims, minCell );
+			for ( int d = 0; d < 3; ++d )
+				ints.put( i + d, blockPos[ d ] * blockSize[ d ] );
+			ints.put( i + 3, 0 );
+		}
+		queue.putUnmapMemory( blockLookup, bytes );
+		queue.finish();
+
+		///////////////////
+
+		final CLImage2d< ByteBuffer > renderTarget = ( CLImage2d< ByteBuffer > ) cl.createImage2d(
+				Buffers.newDirectByteBuffer( width * height ),
+				width,
+				height,
+				new CLImageFormat( ChannelOrder.R, ChannelType.UNSIGNED_INT8 ),
+				Mem.READ_WRITE );
+
+		final AffineTransform3D screenToShiftedSource = new AffineTransform3D();
+		screenToShiftedSource.set(
+				1, 0, 0, - minCell[ 0 ] * blockSize[ 0 ] ,
+				0, 1, 0, - minCell[ 1 ] * blockSize[ 1 ] ,
+				0, 0, 1, - minCell[ 2 ] * blockSize[ 2 ] );
+		screenToShiftedSource.concatenate( sourceToScreen.inverse() );
+		for ( int r = 0; r < 3; ++r )
+			for ( int c = 0; c < 4; ++c )
+				transformMatrix.getBuffer().put( ( float ) screenToShiftedSource.get( r, c ) );
+		transformMatrix.getBuffer().rewind();
+		queue.putWriteBuffer( transformMatrix, true );
+
+		sizes.getBuffer().put( blockSize );
+		sizes.getBuffer().put( 1 );
+		sizes.getBuffer().put( lookupDims );
+		sizes.getBuffer().put( 1 );
+		sizes.getBuffer().rewind();
+		queue.putWriteBuffer( sizes, true );
+
+		final long globalWorkOffsetX = 0;
+		final long globalWorkOffsetY = 0;
+		final long globalWorkSizeX = width;
+		final long globalWorkSizeY = height;
+        final long localWorkSizeX = 0;
+        final long localWorkSizeY = 0;
+        for ( int i = 0; i < 10; ++i )
+        {
+	        long t = System.currentTimeMillis();
+			slice4.rewind().putArg( transformMatrix ).putArg( sizes ).putArg( blockLookup ).putArg( blockTexture.get() ).putArg( renderTarget );
+			queue.put2DRangeKernel( slice4, globalWorkOffsetX, globalWorkOffsetY, globalWorkSizeX, globalWorkSizeY, localWorkSizeX, localWorkSizeY );
+			queue.putReadImage( renderTarget, true ).finish();
+	        t = System.currentTimeMillis() - t;
+	        System.out.println( "t = " + t + " ms" );
+        }
+
+		final byte[] data = new byte[ width * height ];
+		renderTarget.getBuffer().get( data );
+		ImageJFunctions.show( ArrayImgs.unsignedBytes( data, width, height ) );
+
+		renderTarget.release();
+
+		///////////////////
+
+		blockTexture.release();
+		blockLookup.release();
 	}
 
-	public static class CellRequest
-	{
-		private final int[] gridPos;
-
-		public CellRequest( final int[] gridPos )
-		{
-			this.gridPos = gridPos;
-		}
-
-		public CellRequest( final Localizable gridPos )
-		{
-			this.gridPos = new int[ 3 ];
-			gridPos.localize( this.gridPos );
-		}
-
-		public int[] getGridPos()
-		{
-			return gridPos;
-		}
-	}
-
-	private short[] getCellData( final CellRequest request )
+	private short[] getCellData( final int[] cellPos )
 	{
 		final int timepointId = 0; // TODO
 		final int setupId = 0; // TODO
@@ -151,28 +248,22 @@ public class RenderSlice
 		// up to this point, the data should be stored in a per-setup-and-level lookup
 		// ===========================================================================
 
-		final long[] cellGridPosition = new long[] {
-				request.getGridPos()[ 0 ],
-				request.getGridPos()[ 1 ],
-				request.getGridPos()[ 2 ]
-		}; // TODO: implement Localizable?
-
 		final long[] cellMin = new long[ n ];
 		final int[] cellDims  = new int[ n ];
 
 		boolean needsPadding = false;
 		for ( int d = 0; d < n; ++d )
 		{
-			if ( cellGridPosition[ d ] + 1 == numCells[ d ] )
+			if ( cellPos[ d ] + 1 == numCells[ d ] )
 			{
 				needsPadding = true;
 				cellDims[ d ] = borderSize[ d ];
 			}
 			else
 				cellDims[ d ] = cellDimensions[ d ];
-			cellMin[ d ] = cellGridPosition[ d ] * cellDimensions[ d ];
+			cellMin[ d ] = cellPos[ d ] * cellDimensions[ d ];
 		}
-		final int index = IntervalIndexer.positionToIndex( cellGridPosition, numCells );
+		final int index = IntervalIndexer.positionToIndex( cellPos, numCells );
 		final VolatileCell< VolatileShortArray > cell = cellCache.load( index, cellDims, cellMin );
 		final short[] cellData = cell.getData().getCurrentStorageArray();
 
@@ -193,12 +284,28 @@ public class RenderSlice
 		return data;
 	}
 
-	private ArrayList< CellRequest > getRequiredBlocks( final AffineTransform3D sourceToScreen, final int w, final int h, final int dd, final ViewId view )
+	private static class RequiredBlocks
+	{
+		private final ArrayList< int[] > cellPositions;
+
+		private final int[] minCell;
+
+		private final int[] maxCell;
+
+		public RequiredBlocks( final ArrayList< int[] > cellPositions, final int[] minCell, final int[] maxCell )
+		{
+			this.cellPositions = cellPositions;
+			this.minCell = minCell;
+			this.maxCell = maxCell;
+		}
+	}
+
+	private RequiredBlocks getRequiredBlocks( final AffineTransform3D sourceToScreen, final int w, final int h, final int dd, final ViewId view )
 	{
 //		final CachedCellImg< ?, ? > cellImg = (bdv.img.cache.CachedCellImg< ?, ? > ) imgLoader.getVolatileImage( view, 0 );
 		final CachedCellImg< ?, ? > cellImg = (bdv.img.cache.CachedCellImg< ?, ? > ) imgLoader.getImage( view, 0 );
 
-		final ArrayList< CellRequest > requiredCells = new ArrayList< CellRequest >();
+		final ArrayList< int[] > requiredCells = new ArrayList< int[] >();
 
 		final int[] cellDimensions = new int[ 3 ];
 		cellImg.getCells().cellDimensions( cellDimensions );
@@ -270,13 +377,13 @@ public class RenderSlice
 		}
 
 		// stupid implementation for now: check all planes for all cells...
-		// TODO find cell boundaries. t
-		final IntervalIterator gridIter = new IntervalIterator( minCell, maxCell );
-A:		while( gridIter.hasNext() )
+		// TODO: make this more clever
+		final IntervalIterator cellsIter = new IntervalIterator( minCell, maxCell );
+A:		while( cellsIter.hasNext() )
 		{
-			gridIter.fwd();
+			cellsIter.fwd();
 			final double[] pos = new double[ 3 ];
-			gridIter.localize( pos );
+			cellsIter.localize( pos );
 			for ( int d = 0; d < 3; ++d )
 				pos[ d ] *= cellDimensions[ d ];
 			for ( int i = 0; i < sourcePlanes.length; ++i )
@@ -285,12 +392,13 @@ A:		while( gridIter.hasNext() )
 				if ( LinAlgHelpers.dot( pos, plane.getNormal() ) < plane.getDistance() )
 					continue A;
 			}
-//			System.out.println( Util.printCoordinates( gridIter ) );
 
-			requiredCells.add( new CellRequest( gridIter ) );
+			final int[] cellPos = new int[ 3 ];
+			cellsIter.localize( cellPos );
+			requiredCells.add( cellPos );
 		}
 
-		return requiredCells;
+		return new RequiredBlocks( requiredCells, minCell, maxCell );
 	}
 
 	public static HyperPlane transform( final HyperPlane plane, final AffineTransform3D transform )
--- src/main/resources/bdv/cl/slice.cl
+++ src/main/resources/bdv/cl/slice.cl
@@ -79,3 +79,54 @@ __kernel void slice3(
 	uint v = (uint) mad(f, scale, offset);
 	write_imageui(target, (int2)(get_global_id(0),get_global_id(1)), v);
 }
+
+
+
+float3 affine( __constant float4* m, float4 p )
+{
+	return (float3) (dot(m[0], p), dot(m[1], p), dot(m[2], p));
+}
+
+
+__kernel void slice4(
+		__constant float4* transform,
+		__constant uint4* sizes,
+		__global __read_only int3* blockLookup,
+		__read_only image3d_t blocks,
+		__write_only image2d_t target )
+{
+	const int2 opos = {
+			get_global_id(0),
+			get_global_id(1)
+	};
+	const uint3 blockDimensions = sizes[ 0 ].xyz;
+	const uint3 lookupDimensions = sizes[ 1 ].xyz;
+
+	float f = 0;
+	for ( int z = 0; z < 100; ++z )
+	{
+		const float4 gpos = {
+				get_global_id(0),
+				get_global_id(1),
+				z,
+				1
+		};
+		float3 p = affine( transform, gpos );
+		int3 pr = convert_int3( round( p ) );
+		int3 prb = convert_int3( floor( round( p ) / convert_float3( blockDimensions ) ) );
+		if ( ! ( any( prb < (int3) (0, 0, 0) ) || any( prb >= lookupDimensions ) ) )
+		{
+			uint li = prb.x + prb.y * lookupDimensions.x + prb.z * lookupDimensions.x * lookupDimensions.y;
+			int4 pos = (int4) ( blockLookup[ li ] + pr - prb * blockDimensions, 0 );
+			f = fmax(f, read_imageui(blocks, sampler, pos).x);
+//			f = 100;
+		}
+	}
+	const float min = 700;
+	const float max = 3991;
+	const float scale = 255.0 / (max - min + 1);
+	const float offset = - min * scale;
+	uint v = (uint) mad(f, scale, offset);
+	write_imageui( target, opos, v );
+}
+
