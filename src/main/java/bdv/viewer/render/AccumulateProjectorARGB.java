37a3667e7f1b608973972ce00cfb6132b5412211
Stephan Saalfeld <saalfelds@janelia.hhmi.org>
2015-07-09 22:13:06 -0400

    AccumulateProjectorARGB uses alpha for weighted accumulation
    This does not change the result for all cases where alpha = 0xff, i.e.
    the standard case that does not consider alpha.  If alpha is set, it is
    now used to weigh the accumulation which enables per pixel alpha weighted
    addition.

--- src/main/java/bdv/viewer/render/AccumulateProjectorARGB.java
+++ src/main/java/bdv/viewer/render/AccumulateProjectorARGB.java
@@ -24,27 +24,32 @@ public class AccumulateProjectorARGB extends AccumulateProjector< ARGBType, ARGB
 	@Override
 	protected void accumulate( final Cursor< ARGBType >[] accesses, final ARGBType target )
 	{
-		int aSum = 0, rSum = 0, gSum = 0, bSum = 0;
+		double aAcc = 0;
+		int rSum = 0, gSum = 0, bSum = 0;
 		for ( final Cursor< ARGBType > access : accesses )
 		{
 			final int value = access.get().get();
-			final int a = ARGBType.alpha( value );
+			final double a = ARGBType.alpha( value ) / 255.0;
 			final int r = ARGBType.red( value );
 			final int g = ARGBType.green( value );
 			final int b = ARGBType.blue( value );
-			aSum += a;
-			rSum += r;
-			gSum += g;
-			bSum += b;
+
+			/* try this for testing purposes */
+//			if ( r != g && g != b ) a *= 0.125;
+
+			aAcc += a - aAcc * a;
+			rSum += a * r;
+			gSum += a * g;
+			bSum += a * b;
 		}
-		if ( aSum > 255 )
-			aSum = 255;
+		if ( aAcc > 1.0 )
+			aAcc = 1.0;
 		if ( rSum > 255 )
 			rSum = 255;
 		if ( gSum > 255 )
 			gSum = 255;
 		if ( bSum > 255 )
 			bSum = 255;
-		target.set( ARGBType.rgba( rSum, gSum, bSum, aSum ) );
+		target.set( ARGBType.rgba( rSum, gSum, bSum, ( int )( aAcc * 255 ) ) );
 	}
 }
