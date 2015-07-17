8afcb8390b7344358b4d3b1726d1a062a6665489
Stephan Saalfeld <saalfelds@janelia.hhmi.org>
2015-07-16 21:56:19 -0400

    associate composites with sources

--- src/main/java/bdv/viewer/render/AccumulateProjectorFactory.java
+++ src/main/java/bdv/viewer/render/AccumulateProjectorFactory.java
@@ -5,6 +5,7 @@ import java.util.concurrent.ExecutorService;
 
 import net.imglib2.RandomAccessible;
 import net.imglib2.RandomAccessibleInterval;
+import bdv.viewer.Source;
 
 public interface AccumulateProjectorFactory< A >
 {
@@ -12,9 +13,11 @@ public interface AccumulateProjectorFactory< A >
 	 * @param sourceProjectors
 	 *            projectors that will be used to render {@code sources}.
 	 * @param sources
+	 *            sources to identify which channels are being rendered
+	 * @param sourceScreenImages
 	 *            rendered images that will be accumulated into
 	 *            {@code target}.
-	 * @param target
+	 * @param targetScreenImage
 	 *            final image to render.
 	 * @param numThreads
 	 *            how many threads to use for rendering.
@@ -23,8 +26,9 @@ public interface AccumulateProjectorFactory< A >
 	 */
 	public VolatileProjector createAccumulateProjector(
 			final ArrayList< VolatileProjector > sourceProjectors,
-			final ArrayList< ? extends RandomAccessible< A > > sources,
-			final RandomAccessibleInterval< A > target,
+			final ArrayList< Source< ? > > sources,
+			final ArrayList< ? extends RandomAccessible< A > > sourceScreenImages,
+			final RandomAccessibleInterval< A > targetScreenImage,
 			final int numThreads,
 			final ExecutorService executorService );
 }
