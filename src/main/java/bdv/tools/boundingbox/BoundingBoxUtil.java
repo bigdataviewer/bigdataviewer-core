743d56a55f5dd436c1de5d5b1b1ad896d5221c2d
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2015-11-10 13:44:05 +0100

    add some utility methods to compute/manipulate bounding boxes of Sources over time

--- src/main/java/bdv/tools/boundingbox/BoundingBoxUtil.java
+++ src/main/java/bdv/tools/boundingbox/BoundingBoxUtil.java
@@ -32,7 +32,9 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 import net.imglib2.FinalInterval;
+import net.imglib2.FinalRealInterval;
 import net.imglib2.Interval;
+import net.imglib2.RealInterval;
 import net.imglib2.RealPoint;
 import net.imglib2.realtransform.AffineTransform3D;
 import bdv.viewer.Source;
@@ -94,5 +96,92 @@ public class BoundingBoxUtil
 			}
 		}
 		return new FinalInterval( bbMin, bbMax );
+	}
+
+	public static RealInterval getSourcesBoundingBoxReal( final ViewerState state )
+	{
+		return getSourcesBoundingBoxReal( state, 0, state.getNumTimePoints() - 1 );
+	}
+
+	public static RealInterval getSourcesBoundingBoxReal( final ViewerState state, final int minTimepointIndex, final int maxTimepointIndex )
+	{
+		final ArrayList< Source< ? > > sources = new ArrayList< Source< ? > >();
+		for ( final SourceState< ? > source : state.getSources() )
+			sources.add( source.getSpimSource() );
+		return getSourcesBoundingBoxReal( sources, minTimepointIndex, maxTimepointIndex );
+	}
+
+	public static RealInterval getSourcesBoundingBoxReal( final Collection< ? extends Source< ? > > sources, final int minTimepointIndex, final int maxTimepointIndex )
+	{
+		final double[] bbMin = new double[] { Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY };
+		final double[] bbMax = new double[] { Double.NEGATIVE_INFINITY, Double.NEGATIVE_INFINITY, Double.NEGATIVE_INFINITY };
+		final long[] min = new long[ 3 ];
+		final long[] max = new long[ 3 ];
+		final AffineTransform3D sourceTransform = new AffineTransform3D();
+		final RealPoint sourceCorner = new RealPoint( 3 );
+		final RealPoint globalCorner = new RealPoint( 3 );
+		for ( final Source< ? > source : sources )
+		{
+			for ( int t = minTimepointIndex; t <= maxTimepointIndex; ++t )
+			{
+				if ( !source.isPresent( t ) )
+					continue;
+				final Interval sourceInterval = source.getSource( t, 0 );
+				sourceInterval.min( min );
+				sourceInterval.dimensions( max );
+				for ( int d = 0; d < 3; ++d )
+					max[ d ] += min[ d ];
+				source.getSourceTransform( 0, 0, sourceTransform );
+
+				for ( int i = 0; i < 8; ++i )
+				{
+					for ( int d = 0; d < 3; ++d )
+					{
+						final double p = ( i & ( 1 << d ) ) == 0 ? min[ d ] : max[ d ];
+						sourceCorner.setPosition( p, d );
+					}
+					sourceTransform.apply( sourceCorner, globalCorner );
+					for ( int d = 0; d < 3; ++d )
+					{
+						final double p = globalCorner.getDoublePosition( d );
+						if ( p < bbMin[ d ] )
+							bbMin[ d ] = p;
+						if ( p > bbMax[ d ] )
+							bbMax[ d ] = p;
+					}
+				}
+			}
+		}
+		return new FinalRealInterval( bbMin, bbMax );
+	}
+
+	public static RealInterval transformBoundingBoxReal( final RealInterval sourceInterval, final AffineTransform3D transform )
+	{
+		final double[] bbMin = new double[] { Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY };
+		final double[] bbMax = new double[] { Double.NEGATIVE_INFINITY, Double.NEGATIVE_INFINITY, Double.NEGATIVE_INFINITY };
+		final double[] min = new double[ 3 ];
+		final double[] max = new double[ 3 ];
+		final RealPoint sourceCorner = new RealPoint( 3 );
+		final RealPoint globalCorner = new RealPoint( 3 );
+		sourceInterval.realMin( min );
+		sourceInterval.realMax( max );
+		for ( int i = 0; i < 8; ++i )
+		{
+			for ( int d = 0; d < 3; ++d )
+			{
+				final double p = ( i & ( 1 << d ) ) == 0 ? min[ d ] : max[ d ];
+				sourceCorner.setPosition( p, d );
+			}
+			transform.apply( sourceCorner, globalCorner );
+			for ( int d = 0; d < 3; ++d )
+			{
+				final double p = globalCorner.getDoublePosition( d );
+				if ( p < bbMin[ d ] )
+					bbMin[ d ] = p;
+				if ( p > bbMax[ d ] )
+					bbMax[ d ] = p;
+			}
+		}
+		return new FinalRealInterval( bbMin, bbMax );
 	}
 }
