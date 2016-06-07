83ec26aebe951527edbf4ef9e7ddae4d95f7c564
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2016-06-07 23:38:41 +0200

    fix minIntervalSize = 0 for SetupAssignments

--- src/main/java/bdv/tools/brightness/SetupAssignments.java
+++ src/main/java/bdv/tools/brightness/SetupAssignments.java
@@ -6,13 +6,13 @@
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -72,7 +72,7 @@ public class SetupAssignments
 
 	private final double fullRangeMax;
 
-	private final double minIntervalSize;
+	private static final double minIntervalSize = 0;
 
 	public interface UpdateListener
 	{
@@ -81,21 +81,14 @@ public class SetupAssignments
 
 	private UpdateListener updateListener;
 
-	@Deprecated
-	public SetupAssignments( final ArrayList< ConverterSetup > converterSetups, final int fullRangeMin, final int fullRangeMax )
-	{
-		this( converterSetups, fullRangeMin, fullRangeMax, 1 );
-	}
-
 	/**
 	 *
 	 * @param converterSetups
 	 * @param fullRangeMin
 	 * @param fullRangeMax
 	 */
-	public SetupAssignments( final ArrayList< ConverterSetup > converterSetups, final double fullRangeMin, final double fullRangeMax, final double minIntervalSize )
+	public SetupAssignments( final ArrayList< ConverterSetup > converterSetups, final double fullRangeMin, final double fullRangeMax )
 	{
-		this.minIntervalSize = minIntervalSize;
 		setups = new ArrayList< ConverterSetup >( converterSetups );
 		minMaxGroups = new ArrayList< MinMaxGroup >();
 		setupToGroup = new HashMap< ConverterSetup, MinMaxGroup >();
