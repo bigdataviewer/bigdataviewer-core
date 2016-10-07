ca9a80bc32d14a06fe8f1c763fa384ad024a00cc
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2016-10-07 16:03:43 +0200

    javadoc

--- src/main/java/bdv/cache/VolatileCacheEntry.java
+++ src/main/java/bdv/cache/VolatileCacheEntry.java
@@ -1,5 +1,15 @@
 package bdv.cache;
 
+/**
+ * A {@link VolatileCache} entry, comprising a key/value pair.
+ *
+ * @param <K>
+ *            the key type
+ * @param <V>
+ *            the value type
+ *
+ * @author Tobias Pietzsch &lt;tobias.pietzsch@gmail.com&gt;
+ */
 public interface VolatileCacheEntry< K, V extends VolatileCacheValue >
 {
 	public K getKey();
--- src/main/java/bdv/cache/VolatileCacheValue.java
+++ src/main/java/bdv/cache/VolatileCacheValue.java
@@ -2,11 +2,18 @@ package bdv.cache;
 
 /**
  * Value (or placeholder for value) that is stored in a cache and may be
- * initially invalid.
+ * initially invalid. The value can only go from invalid state to valid, never
+ * vice-versa.
  *
  * @author Tobias Pietzsch &lt;tobias.pietzsch@gmail.com&gt;
  */
 public interface VolatileCacheValue
 {
+	/**
+	 * Check whether the value is currently valid. The value can only go from
+	 * invalid state to valid, never vice-versa.
+	 *
+	 * @return whether the value is valid.
+	 */
 	public boolean isValid();
 }
