e246512ead9252813216661654609a3075c423cf
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2016-12-11 22:00:44 +0100

    split finalize-queue into separate class. One queue is shared by many WeakSoftCaches

--- src/main/java/bdv/cache/WeakSoftCacheImp.java
+++ src/main/java/bdv/cache/WeakSoftCacheImp.java
@@ -30,7 +30,6 @@
 package bdv.cache;
 
 import java.lang.ref.Reference;
-import java.lang.ref.ReferenceQueue;
 import java.lang.ref.SoftReference;
 import java.lang.ref.WeakReference;
 import java.util.concurrent.ConcurrentHashMap;
@@ -48,101 +47,57 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 public class WeakSoftCacheImp< K, V > implements WeakSoftCache< K, V >
 {
-	public static final int MAX_PER_FRAME_FINALIZE_ENTRIES = 500;
-
-	private final ConcurrentHashMap< K, Reference< V > > softReferenceCache = new ConcurrentHashMap<>();
-
-	private final ReferenceQueue< V > finalizeQueue = new ReferenceQueue<>();
+	private static WeakSoftCacheFinalizeQueue sharedQueue = new WeakSoftCacheFinalizeQueue();
 
 	WeakSoftCacheImp()
-	{}
+	{
+		this( sharedQueue );
+	}
+
+	private final ConcurrentHashMap< K, Reference< V > > map = new ConcurrentHashMap<>();
+
+	private final WeakSoftCacheFinalizeQueue queue;
+
+	public WeakSoftCacheImp( final WeakSoftCacheFinalizeQueue finalizeQueue )
+	{
+		this.queue = finalizeQueue;
+	}
 
 	@Override
 	public void putWeak( final K key, final V value )
 	{
-		softReferenceCache.put( key, new MyWeakReference<>( key, value, finalizeQueue ) );
+		map.put( key, queue.createWeakReference( key, value, map ) );
 	}
 
 	@Override
 	public void putSoft( final K key, final V value )
 	{
-		softReferenceCache.put( key, new MySoftReference<>( key, value, finalizeQueue ) );
+		map.put( key, queue.createSoftReference( key, value, map ) );
 	}
 
 	@Override
 	public V get( final Object key )
 	{
-		final Reference< V > ref = softReferenceCache.get( key );
+		final Reference< V > ref = map.get( key );
 		return ref == null ? null : ref.get();
 	}
 
 	@Override
 	public void invalidateAll()
 	{
-		for ( final Reference< ? > ref : softReferenceCache.values() )
+		for ( final Reference< ? > ref : map.values() )
 			ref.clear();
-		softReferenceCache.clear();
+		map.clear();
 	}
 
 	/**
-	 * Remove references from the cache that have been garbage-collected.
-	 * To avoid long run-times, per call to {@code cleanUp()}, at most
+	 * Remove references from the cache that have been garbage-collected. To
+	 * avoid long run-times, per call to {@code cleanUp()}, at most
 	 * {@link #MAX_PER_FRAME_FINALIZE_ENTRIES} are processed.
 	 */
 	@Override
 	public void cleanUp()
 	{
-		synchronized ( softReferenceCache )
-		{
-			for ( int i = 0; i < MAX_PER_FRAME_FINALIZE_ENTRIES; ++i )
-			{
-				final Reference< ? > poll = finalizeQueue.poll();
-				if ( poll == null )
-					break;
-				final Object key = ( ( GetKey< ? > ) poll ).getKey();
-				final Reference< ? > ref = softReferenceCache.get( key );
-				if ( ref == poll )
-					softReferenceCache.remove( key );
-			}
-		}
-	}
-
-	private static interface GetKey< K >
-	{
-		public K getKey();
-	}
-
-	private static class MySoftReference< K, V > extends SoftReference< V > implements GetKey< K >
-	{
-		private final K key;
-
-		public MySoftReference( final K key, final V referent, final ReferenceQueue< ? super V > q )
-		{
-			super( referent, q );
-			this.key = key;
-		}
-
-		@Override
-		public K getKey()
-		{
-			return key;
-		}
-	}
-
-	private static class MyWeakReference< K, V > extends WeakReference< V > implements GetKey< K >
-	{
-		private final K key;
-
-		public MyWeakReference( final K key, final V referent, final ReferenceQueue< ? super V > q )
-		{
-			super( referent, q );
-			this.key = key;
-		}
-
-		@Override
-		public K getKey()
-		{
-			return key;
-		}
+		queue.cleanUp();
 	}
 }
