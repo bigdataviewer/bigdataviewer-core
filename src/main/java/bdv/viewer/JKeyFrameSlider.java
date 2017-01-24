d81d5f33e96ae1c8d651320b74f203cc42dd1436
Moritz Riebe <moritz.riebe@mz-solutions.de>
2017-01-24 15:37:48 +0100

    JKeyFrame Slider with flagged KeyFrames and Context-/Popup-Menu

--- src/main/java/bdv/viewer/JKeyFrameSlider.java
+++ src/main/java/bdv/viewer/JKeyFrameSlider.java
@@ -28,18 +28,42 @@ package bdv.viewer;
 import bdv.tools.bookmarks.bookmark.DynamicBookmark;
 import bdv.tools.bookmarks.bookmark.KeyFrame;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JPopupMenu;
 import javax.swing.JSlider;
+import javax.swing.SwingUtilities;
 import javax.swing.plaf.basic.BasicSliderUI;
 
 /**
- *
- * @author Riebe, Moritz (moritz.riebe@mz-solutions.de)
+ * Extends the {@link JSlider}-Component with the ability to flag single {@link KeyFrame}'s.
+ * 
+ * @author  Riebe, Moritz (moritz.riebe@mz-solutions.de)
  */
 public final class JKeyFrameSlider extends JSlider {
     
+    /** KeyFrame-Flag (red-Line) Width. */
+    private static final int KF_FLAG_WIDTH = 1;
+    
+    private static final Color CL_KF_FLAG_NORMAL = Color.RED;
+    private static final Color CL_KF_FLAG_HOVER = CL_KF_FLAG_NORMAL.darker();
+    
+    private static enum KeyFrameFlagState {
+        NORMAL,
+        HOVER
+    }
+    
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    
+    /** My Context-Menu. */
+    private final KeyFramePopupMenu popupMenu = new KeyFramePopupMenu();
+    
     /** Current dynamic bookmark or null if no bookmark is selected. */
     private DynamicBookmark bookmark = null;
+    
+    private int currentHoverTimepoint = -1;
 
     public JKeyFrameSlider() {
         this(0, 100, 50);
@@ -51,6 +75,17 @@ public final class JKeyFrameSlider extends JSlider {
 
     public JKeyFrameSlider(int min, int max, int value) {
         super(min, max, value);
+        initComponent();
+    }
+    
+    private void initComponent() {
+        addMouseListener(new MouseHoverEventAdapter());
+        addMouseMotionListener(new MouseHoverEventAdapter());
+        
+        setMinimumSize(new Dimension((int) getMinimumSize().getWidth(), 26));
+        setPreferredSize(new Dimension((int) getPreferredSize().getWidth(), 26));
+        
+        setFocusable(false);
     }
     
     /**
@@ -60,7 +95,52 @@ public final class JKeyFrameSlider extends JSlider {
      */
     public void setDynamicBookmarks(DynamicBookmark bookmark) {
         this.bookmark = bookmark;
+        this.currentHoverTimepoint = -1;
         repaint();
+    }
+
+    /**
+     * Returns the specific {@link KeyFramePopupMenu} of this component.
+     * 
+     * @return  Returns always the same instance of {@link KeyFramePopupMenu}, never {@code null}.
+     */
+    public KeyFramePopupMenu getKeyFramePopupMenuPopupMenu() {
+        return this.popupMenu;
+    }
+    
+    @Override
+    public JPopupMenu getComponentPopupMenu() {
+        // Needs to return null! If a popup menu instance is returned, it will be used... without
+        // our mouse-events depending on selected key-frames
+        return null;
+    }
+
+    /**
+     * Setting the popup menu is not allowed for this component!.
+     * 
+     * @param popup     - no -
+     */
+    @Override
+    public void setComponentPopupMenu(JPopupMenu popup) {
+        throw new IllegalStateException(JKeyFrameSlider.class.getSimpleName() + " cannot be set");
+    }
+    
+    private boolean isKeyFrameFlagMouseHover() {
+        return currentHoverTimepoint != -1;
+    }
+    
+    private KeyFrame getKeyFrameFlagMouseOver() {
+        if (isKeyFrameFlagMouseHover() == false) {
+            return null;
+        }
+        
+        for (KeyFrame singleFrame : bookmark.getFrameSet()) {
+            if (singleFrame.getTimepoint() == this.currentHoverTimepoint) {
+                return singleFrame;
+            }
+        }
+        
+        return null;
     }
     
     @Override
@@ -71,24 +151,102 @@ public final class JKeyFrameSlider extends JSlider {
             return;
         }
         
-        final int width = getWidth();
-        final int height = getHeight();
-        
-        final Color clTimepointFlag = Color.RED;
-        
-        g.setColor(clTimepointFlag);
-        
         for (int singleTimepoint : convertKeyFramesAsArray()) {
-            final int posX = (int) (width * ((double) singleTimepoint / (double) width));
+            final int posX = determineSliderXPositionOf(singleTimepoint);
             
-            g.fillRect(posX, 0, 2, height);
+            if (singleTimepoint == this.currentHoverTimepoint) {
+                paintKeyFrameFlag(g, posX, KeyFrameFlagState.HOVER);
+            } else {
+                paintKeyFrameFlag(g, posX, KeyFrameFlagState.NORMAL);
+            }
         }
         
         ((BasicSliderUI) getUI()).paintThumb(g);
     }
     
+    private void paintKeyFrameFlag(Graphics g, int sliderPositionX, KeyFrameFlagState flagState) {
+        if (flagState == KeyFrameFlagState.NORMAL) {
+            g.setColor(CL_KF_FLAG_NORMAL);
+            g.fillRect(sliderPositionX, 0, KF_FLAG_WIDTH, getHeight());
+        } else {
+            g.setColor(CL_KF_FLAG_HOVER);
+            g.fillRect(sliderPositionX - 1, 0, KF_FLAG_WIDTH + 2, getHeight());
+        }
+    }
+    
+    private void determineKeyFrameHoverFlag(int inputComponentXCoord) {
+        for (int anyTimepoint : convertKeyFramesAsArray()) {
+            final int anyValidPosX = determineSliderXPositionOf(anyTimepoint);
+            
+            final int lowerBound = anyValidPosX;
+            final int upperBound = anyValidPosX + KF_FLAG_WIDTH + 1;
+            
+            final int mouseX = inputComponentXCoord;
+            
+            if (mouseX >= lowerBound && mouseX <= upperBound) {
+                this.currentHoverTimepoint = anyTimepoint;
+                return;
+            }
+        }
+        
+        this.currentHoverTimepoint = -1;
+    }
+    
     private int[] convertKeyFramesAsArray() {
         return bookmark.getFrameSet().stream().mapToInt(KeyFrame::getTimepoint).toArray();
+    }
+    
+    private int determineSliderXPositionOf(int timepoint) {
+        final double sliderWidth = getWidth();
+        return (int) (sliderWidth * ((double) timepoint / sliderWidth));
+    }
+    
+    private class MouseHoverEventAdapter extends MouseAdapter {
+
+        @Override
+        public void mouseMoved(MouseEvent e) {
+            updateComponent(e);
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+            updateComponent(e);
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+            updateComponent(e);
+        }
+        
+        private void updateComponent(MouseEvent event) {
+            if (bookmark != null) {
+                determineKeyFrameHoverFlag(event.getX());
+                SwingUtilities.invokeLater(JKeyFrameSlider.this::repaint);
+            }
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+            maybeTriggerPopupMenu(e);
+        }
+
+        @Override
+        public void mousePressed(MouseEvent e) {
+            maybeTriggerPopupMenu(e);
+        }
+        
+        private void maybeTriggerPopupMenu(MouseEvent event) {
+            if (event.isPopupTrigger() ) {
+                if (isKeyFrameFlagMouseHover()) {
+                    popupMenu.setKeyFrameFlagSelected(getKeyFrameFlagMouseOver());
+                } else {
+                    popupMenu.setKeyFrameFlagSelected(null /* no selected frame */);
+                }
+                
+                popupMenu.show(JKeyFrameSlider.this, event.getX(), event.getY());
+            }
+        }
+        
     }
     
 }
---
+++ src/main/java/bdv/viewer/KeyFramePopupMenu.java
@@ -0,0 +1,111 @@
+/*
+ * Copyright (c) 2017, Fiji
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package bdv.viewer;
+
+import bdv.tools.bookmarks.bookmark.KeyFrame;
+import java.awt.event.ActionEvent;
+import java.util.Optional;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JPopupMenu;
+import javax.swing.JSeparator;
+
+/**
+ * Context-Menu for the {@link JKeyFrameSlider}-Component.
+ * 
+ * @author Riebe, Moritz (moritz.riebe@mz-solutions.de)
+ */
+public final class KeyFramePopupMenu extends JPopupMenu {
+    
+    private final JMenuItem itemSelectKF = new JMenuItem();
+    private final JMenuItem itemRemoveKF = new JMenuItem();
+    
+    private final JMenuItem itemOpenBookmarkManager = new JMenuItem();
+    private final JSeparator sepContextFromNonContext = new Separator();
+    
+    /** User-selected {@link KeyFrame} or {@code null} if no frame is selected. */
+    private volatile KeyFrame selectedKeyFrame = null;
+    
+    KeyFramePopupMenu() {
+        initComponents();
+    }
+    
+    private void initComponents() {
+        add(itemSelectKF);
+        add(itemRemoveKF);
+        add(sepContextFromNonContext);
+        add(itemOpenBookmarkManager);
+        
+        itemSelectKF.setText("Select Key-Frame");
+        itemSelectKF.addActionListener(this::onActionPerformedEventItemSelectKF);
+        
+        itemRemoveKF.setText("Remove Key-Frame-Flag");
+        itemRemoveKF.addActionListener(this::onActionPerformedEventItemRemoveFK);
+        
+        itemOpenBookmarkManager.setText("Open Bookmark Management..");
+        itemOpenBookmarkManager.addActionListener(this::onActionPerformedEventOpenBookmarkManager);
+        
+        setKeyFrameFlagSelected(null);
+    }
+    
+    protected void setKeyFrameFlagSelected(KeyFrame selectedKeyFrameOrNull) {
+        final boolean visibleKeyFrameActions = (null != selectedKeyFrameOrNull);
+        
+        this.itemSelectKF.setVisible(visibleKeyFrameActions);
+        this.itemRemoveKF.setVisible(visibleKeyFrameActions);
+        this.sepContextFromNonContext.setVisible(visibleKeyFrameActions);
+        
+        this.selectedKeyFrame = selectedKeyFrameOrNull;
+    }
+    
+    /**
+     * Returns current user-selected {@link KeyFrame} or nothing if no frame is selected.
+     * @return  {@code Optional.empty()} if no frame is selected or {@code Optional.of(..)}.
+     */
+    protected Optional<KeyFrame> getSelectedKeyFrame() {
+        return Optional.ofNullable(this.selectedKeyFrame);
+    }
+    
+    private void testAction() {
+        JOptionPane.showMessageDialog(getParent(),
+                "Selected KeyFrame Timepoint: " + getSelectedKeyFrame().get().getTimepoint(),
+                "KeyFrame",
+                JOptionPane.INFORMATION_MESSAGE);
+    }
+    
+    private void onActionPerformedEventItemSelectKF(ActionEvent event) {
+        testAction();
+    }
+    
+    private void onActionPerformedEventItemRemoveFK(ActionEvent event) {
+        testAction();
+    }
+    
+    private void onActionPerformedEventOpenBookmarkManager(ActionEvent event) {
+        // no testAction() -> event may be called when no frame is selected
+    }
+    
+}
