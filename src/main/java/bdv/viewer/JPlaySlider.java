83183f78b56b9942959dea4b5c4659465142a992
Max Kleinhenz <max.kleinhenz@outlook.com>
2017-03-29 09:54:45 +0200

    JPlaySlider: set focusable to false

--- src/main/java/bdv/viewer/JPlaySlider.java
+++ src/main/java/bdv/viewer/JPlaySlider.java
@@ -6,35 +6,38 @@ import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JSlider;
 
-public class JPlaySlider extends JSlider{
+public class JPlaySlider extends JSlider
+{
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
-	public JPlaySlider() {
-        putClientProperty("JSlider.isFilled", Boolean.FALSE);
-        
-        setOpaque(false);
-        
-        setMaximum(8);
-		setMinimum(-8);
-    	setValue(0);
-    	
-		setSnapToTicks(true);
-		
-		setPaintTicks(true);
-		setPaintLabels(true);
-		setMinorTickSpacing(1);
-		setMajorTickSpacing(4);
-		
-		Hashtable<Integer, JComponent> labels = new Hashtable<>();
-		labels.put(getMinimum(), new JLabel ( "<<" ));
-		labels.put(getMinimum()/2, new JLabel ( "<" ));
-		labels.put(0, new JLabel ( "||" ));
-		labels.put(getMaximum()/2, new JLabel ( ">" ));
-		labels.put(getMaximum(), new JLabel ( ">>" ));
-		setLabelTable(labels);
-    }
+
+	public JPlaySlider()
+	{
+		putClientProperty( "JSlider.isFilled", Boolean.FALSE );
+
+		setOpaque( false );
+		setFocusable( false );
+
+		setMaximum( 8 );
+		setMinimum( -8 );
+		setValue( 0 );
+
+		setSnapToTicks( true );
+
+		setPaintTicks( true );
+		setPaintLabels( true );
+		setMinorTickSpacing( 1 );
+		setMajorTickSpacing( 4 );
+
+		Hashtable< Integer, JComponent > labels = new Hashtable<>();
+		labels.put( getMinimum(), new JLabel( "<<" ) );
+		labels.put( getMinimum() / 2, new JLabel( "<" ) );
+		labels.put( 0, new JLabel( "||" ) );
+		labels.put( getMaximum() / 2, new JLabel( ">" ) );
+		labels.put( getMaximum(), new JLabel( ">>" ) );
+		setLabelTable( labels );
+	}
 }
