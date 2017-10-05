46863d3ebaf542d67e17108eed8dff1b5fe1b87f
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2017-10-05 11:01:00 +0200

    formatting and TODOs

--- src/main/java/bdv/tools/bookmarks/bookmark/KeyFrame.java
+++ src/main/java/bdv/tools/bookmarks/bookmark/KeyFrame.java
@@ -3,51 +3,61 @@ package bdv.tools.bookmarks.bookmark;
 import java.util.Objects;
 import net.imglib2.realtransform.AffineTransform3D;
 
-public class KeyFrame {
+public class KeyFrame
+{
+	private final int timepoint;
 
-	private int timepoint;
-	private AffineTransform3D transform;
+	private AffineTransform3D transform; // TODO: final?
 
-	public KeyFrame(int timepoint, AffineTransform3D transform) {
+	public KeyFrame( int timepoint, AffineTransform3D transform )
+	{
 		this.timepoint = timepoint;
 		this.transform = transform;
 	}
-	
-	protected KeyFrame(KeyFrame k){
+
+	protected KeyFrame( KeyFrame k )
+	{
 		this.timepoint = k.timepoint;
 		this.transform = k.transform.copy();
 	}
 
-	public int getTimepoint() {
+	public int getTimepoint()
+	{
 		return timepoint;
 	}
 
-	public AffineTransform3D getTransform() {
+	public AffineTransform3D getTransform()
+	{
 		return transform;
 	}
 
-	public void setTransform(AffineTransform3D transform) {
-		this.transform = transform;
+	public void setTransform( AffineTransform3D transform )
+	{
+		this.transform = transform; // TODO: this.transform.set( transform ); ???
 	}
-	
-	public KeyFrame copy(){
-		return new KeyFrame(this);
+
+	public KeyFrame copy()
+	{
+		return new KeyFrame( this );
 	}
-	
-    @Override
-	public boolean equals(Object other){
-		if(other instanceof KeyFrame){
-			if (getClass() == other.getClass()) {
-				KeyFrame k = (KeyFrame) other;
+
+	@Override
+	public boolean equals( Object other )
+	{
+		if ( other instanceof KeyFrame )
+		{
+			if ( getClass() == other.getClass() )
+			{
+				KeyFrame k = ( KeyFrame ) other;
 				return this.timepoint == k.getTimepoint();
 			}
 		}
 		return false;
 	}
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(getClass(), this.timepoint);
-    }
-    
+	@Override
+	public int hashCode()
+	{
+		return Objects.hash( getClass(), this.timepoint );
+	}
 }
