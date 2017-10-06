c20a09d728ad14074f231d341541c2f2de570fa8
Stephan Saalfeld <saalfelds@janelia.hhmi.org>
2017-10-06 18:45:16 -0400

    made settings loading and saving public

--- src/main/java/bdv/BigDataViewer.java
+++ src/main/java/bdv/BigDataViewer.java
@@ -7,13 +7,13 @@
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -573,7 +573,7 @@ public class BigDataViewer
 		return false;
 	}
 
-	protected void saveSettings()
+	public void saveSettings()
 	{
 		fileChooser.setSelectedFile( proposedSettingsFile );
 		final int returnVal = fileChooser.showSaveDialog( null );
@@ -654,7 +654,7 @@ public class BigDataViewer
 		return conf;
 	}
 
-	protected void loadSettings()
+	public void loadSettings()
 	{
 		fileChooser.setSelectedFile( proposedSettingsFile );
 		final int returnVal = fileChooser.showOpenDialog( null );
