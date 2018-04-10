ba14aae0d22c40b1059b387cbfb9fb3b63873291
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2018-04-10 18:38:27 +0200

    Replace linkType() reflection hack by PrimitiveTypeInfo.createLinkedType()

--- src/main/java/bdv/img/cache/VolatileCachedCellImg.java
+++ src/main/java/bdv/img/cache/VolatileCachedCellImg.java
@@ -1,7 +1,5 @@
 package bdv.img.cache;
 
-import java.lang.reflect.InvocationTargetException;
-
 import bdv.cache.CacheControl;
 import bdv.img.cache.VolatileCachedCellImg.VolatileCachedCells;
 import net.imglib2.Volatile;
@@ -17,6 +15,7 @@ import net.imglib2.img.cell.CellGrid;
 import net.imglib2.img.cell.LazyCellImg;
 import net.imglib2.img.list.AbstractLongListImg;
 import net.imglib2.type.NativeType;
+import net.imglib2.type.PrimitiveTypeInfo;
 
 /**
  * A {@link LazyCellImg} for {@link Volatile} accesses. The only difference to
@@ -42,14 +41,10 @@ public class VolatileCachedCellImg< T extends NativeType< T >, A >
 	public VolatileCachedCellImg( final CellGrid grid, final T type, final CacheHints cacheHints, final Get< Cell< A > > get )
 	{
 		super( grid, new VolatileCachedCells<>( grid.getGridDimensions(), get, cacheHints ), type.getEntitiesPerPixel() );
-		try
-		{
-			LazyCellImg.linkType( type, this );
-		}
-		catch ( NoSuchMethodException | SecurityException | InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e )
-		{
-			throw new RuntimeException( e );
-		}
+
+		@SuppressWarnings( "unchecked" )
+		final PrimitiveTypeInfo< T, ? super A > info = ( PrimitiveTypeInfo< T, ? super A > ) type.getPrimitiveTypeInfo();
+		setLinkedType( info.createLinkedType( this ) );
 	}
 
 	/**
