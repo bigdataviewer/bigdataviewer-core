3ede3dd78c68a9e02445784864db2c8cd6b31f89
tpietzsch <tobias.pietzsch@gmail.com>
2019-12-19 12:01:18 +0100

    WIP

--- src/main/java/bdv/ui/SplitPaneOneTouchExpandAnimator.java
+++ src/main/java/bdv/ui/SplitPaneOneTouchExpandAnimator.java
@@ -12,64 +12,54 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 
-import static bdv.ui.SplitPaneOneTouchExpandAnimator.AnimationType.ACTIVATE_COLLAPS;
-import static bdv.ui.SplitPaneOneTouchExpandAnimator.AnimationType.ACTIVATE_EXPAND;
-import static bdv.ui.SplitPaneOneTouchExpandAnimator.AnimationType.DEACTIVATE_COLLAPS;
-import static bdv.ui.SplitPaneOneTouchExpandAnimator.AnimationType.DEACTIVATE_EXPAND;
+import static bdv.ui.SplitPaneOneTouchExpandAnimator.AnimationType.SHOW_COLLAPSE;
+import static bdv.ui.SplitPaneOneTouchExpandAnimator.AnimationType.SHOW_EXPAND;
+import static bdv.ui.SplitPaneOneTouchExpandAnimator.AnimationType.HIDE_COLLAPSE;
+import static bdv.ui.SplitPaneOneTouchExpandAnimator.AnimationType.HIDE_EXPAND;
 import static bdv.ui.SplitPaneOneTouchExpandAnimator.AnimationType.NONE;
 
 public class SplitPaneOneTouchExpandAnimator implements OverlayAnimator, MouseMotionListener, MouseListener
 {
-
 	private final BufferedImage leftarrow;
-
 	private final BufferedImage rightarrow;
-
-	private final SplitPanel splitPanel;
-
 	private final int imgw;
-
 	private final int imgh;
-
-	private int viewPortWidth;
-
-	private int viewPortHeight;
-
-	private int mouseX;
-
-	private int mouseY;
-
-	private boolean isAnimating;
-
-	private long last_time;
-
-	private boolean collapsed;
+	private final double animationSpeed = 0.09;
+	private final float backgroundAlpha = 0.65f;
 
 	private float alpha = 1.0f;
 
-	private int keyFrame;
+	private int viewPortWidth;
+	private int viewPortHeight;
 
-	private final double animationSpeed = 0.009;
 
-	private final float backgroundAlpha = 0.65f;
 
-	private PaintState paintState;
+	private final SplitPanel splitPanel;
+	private boolean collapsed;
+
+
+	private int mouseX;
+	private int mouseY;
+
+
+	private boolean isAnimating;
+	private long last_time;
 
 	public SplitPaneOneTouchExpandAnimator( final SplitPanel viewer ) throws IOException
 	{
-		rightarrow = ImageIO.read( SplitPaneOneTouchExpandAnimator.class.getResource( "rightdoublearrow_tiny.png" ) );
-		leftarrow = ImageIO.read( SplitPaneOneTouchExpandAnimator.class.getResource( "leftdoublearrow_tiny.png" ) );
+		rightarrow = ImageIO.read( SplitPaneOneTouchExpandAnimator.class.getResource( "rightdoublearrow_tiny.png" ) ); // TODO: use ImageIcon instead?
+		leftarrow = ImageIO.read( SplitPaneOneTouchExpandAnimator.class.getResource( "leftdoublearrow_tiny.png" ) ); // TODO: use ImageIcon instead?
 		this.imgw = leftarrow.getWidth();
-		this.imgh = leftarrow.getHeight() + 2;
+		this.imgh = leftarrow.getHeight() + 2; // TODO: why +2?
 		this.splitPanel = viewer;
 	}
 
 	enum AnimationType
 	{
-		ACTIVATE_EXPAND,
-		DEACTIVATE_EXPAND,
-		ACTIVATE_COLLAPS,
-		DEACTIVATE_COLLAPS,
+		SHOW_EXPAND,
+		HIDE_EXPAND,
+		SHOW_COLLAPSE,
+		HIDE_COLLAPSE,
 		NONE
 	}
 
@@ -86,23 +76,23 @@ public class SplitPaneOneTouchExpandAnimator implements OverlayAnimator, MouseMo
 			last_time = time;
 			switch ( animationType )
 			{
-			case ACTIVATE_EXPAND:
-				animation = new ActivateExpand();
+			case SHOW_EXPAND:
+				animation = new ShowExpandButton( animation );
 				isAnimating = true;
 				animationType = NONE;
 				break;
-			case DEACTIVATE_EXPAND:
-				animation = new DeactivateExpand();
+			case HIDE_EXPAND:
+				animation = new HideExpandButton( animation );
 				isAnimating = true;
 				animationType = NONE;
 				break;
-			case ACTIVATE_COLLAPS:
-				animation = new ActivateCollapse();
+			case SHOW_COLLAPSE:
+				animation = new ShowCollapseButton();
 				isAnimating = true;
 				animationType = NONE;
 				break;
-			case DEACTIVATE_COLLAPS:
-				animation = new DeactivateCollapse();
+			case HIDE_COLLAPSE:
+				animation = new HideCollapseButton();
 				isAnimating = true;
 				animationType = NONE;
 				break;
@@ -126,12 +116,20 @@ public class SplitPaneOneTouchExpandAnimator implements OverlayAnimator, MouseMo
 			paint( g, paintState );
 	}
 
-	interface Animation
+	@Override
+	public boolean isComplete()
 	{
-		PaintState animate( final long delta_time );
-
-		boolean isComplete();
+		return false;
 	}
+
+	@Override
+	public boolean requiresRepaint()
+	{
+		return isAnimating;
+	}
+
+
+	// == PAINTING ===========================================================
 
 	private static class PaintState
 	{
@@ -149,18 +147,78 @@ public class SplitPaneOneTouchExpandAnimator implements OverlayAnimator, MouseMo
 		}
 	}
 
+	private PaintState paintState;
+
 	private void paint( final Graphics2D g, final PaintState state )
 	{
 		final int imgX = viewPortWidth - ( int ) ( imgw * state.imgRatio + 10 * state.bumpRatio );
 		final int bgX = viewPortWidth - ( int ) ( imgw * state.bgRatio + 10 * state.bumpRatio );
+		final int y = ( viewPortHeight - imgh ) / 2;
 
-		drawBackground( g, bgX, state.alpha );
-		drawImg( g, collapsed ? leftarrow : rightarrow, imgX, state.alpha );
+		drawBackground( g, bgX, y, state.alpha );
+		drawImg( g, collapsed ? leftarrow : rightarrow, imgX, y, state.alpha );
+	}
+
+	private void drawBackground( final Graphics2D g, final int x, final int y, final float alpha )
+	{
+		final int width = imgw + 60;
+		final int height = imgh;
+
+		g.setColor( new Color( 0, 0, 0, Math.min( alpha, backgroundAlpha ) ) );
+		g.fillRoundRect( x, y, width, height, 25, 25 );
+	}
+
+	private void drawImg( final Graphics2D g, final BufferedImage img, final int x, final int y, final float alpha )
+	{
+		Composite oldComposite = g.getComposite();
+		final AlphaComposite alcom = AlphaComposite.getInstance( AlphaComposite.SRC_OVER, alpha );
+		g.setComposite( alcom );
+		g.drawImage( img, x, y, null );
+		g.setComposite(oldComposite);
+	}
+
+
+	// == ANIMATOR HELPERS ===================================================
+
+	/**
+	 * Cosine shape acceleration/ deceleration curve  of linear [0,1]
+	 */
+	private double cos( final double t )
+	{
+		return 0.5 - 0.5 * Math.cos( Math.PI * t );
+	}
+
+	private void updateAlpha( final boolean fadeIn, final long delta_time )
+	{
+		if ( fadeIn )
+		{
+			alpha += ( 1 / 250.0 ) * delta_time;
+			alpha = Math.min( 1, alpha );
+		}
+		else
+		{
+			alpha -= ( 1 / 250.0 ) * delta_time;
+			alpha = Math.max( 0.25f, alpha );
+		}
+	}
+
+
+	// == ANIMATORS ==========================================================
+
+	interface Animation
+	{
+		PaintState animate( final long delta_time );
+
+		boolean isComplete();
 	}
 
 	private Animation animation;
 
-	private class DeactivateCollapse implements Animation
+
+
+	// =======================================================================
+
+	private class HideCollapseButton implements Animation
 	{
 		private boolean complete = false;
 
@@ -169,9 +227,6 @@ public class SplitPaneOneTouchExpandAnimator implements OverlayAnimator, MouseMo
 		{
 			// Fade-out
 			updateAlpha( false, delta_time );
-
-			// No move animation required
-			final int x = viewPortWidth - imgw;
 
 			complete = alpha <= 0.25;
 
@@ -185,64 +240,86 @@ public class SplitPaneOneTouchExpandAnimator implements OverlayAnimator, MouseMo
 		}
 	}
 
-	private class ActivateCollapse implements Animation
+
+
+	// =======================================================================
+
+	private class ShowCollapseButton implements Animation
 	{
+		// TODO (?)
+		// Slide image 10px to the left
+		private final double bumpAnimationSpeed = animationSpeed / 10;
+
 		private int keyFrame = 0;
 
 		// ratio to which the image is slid left (0 = all the way right, 1 = all the way left)
-		private double expandRatio = 0;
+		private double bumpRatio = 0;
+
+		private boolean complete = false;
 
 		@Override
 		public PaintState animate( final long delta_time )
 		{
-			// TODO : should become new animation speed
-			// Slide image 10px to the left
-			final double expandAnimationSpeed = animationSpeed / 10;
-
 			if ( keyFrame == 0 )
 			{
 				// Slide image to the left
-				expandRatio += expandAnimationSpeed * delta_time;
-				expandRatio = Math.min( 1, Math.max( 0, expandRatio ) );
-				if ( expandRatio == 1 )
+				bumpRatio += bumpAnimationSpeed * delta_time;
+				bumpRatio = Math.min( 1, Math.max( 0, bumpRatio ) );
+				if ( bumpRatio == 1 )
 					keyFrame = 1;
 
 			}
 			else if ( keyFrame == 1 )
 			{
 				// Slide image back to the initial position
-				expandRatio -= expandAnimationSpeed * delta_time;
-				expandRatio = Math.min( 1, Math.max( 0, expandRatio ) );
-				if ( expandRatio == 0 )
+				bumpRatio -= bumpAnimationSpeed * delta_time;
+				bumpRatio = Math.min( 1, Math.max( 0, bumpRatio ) );
+				if ( bumpRatio == 0 )
 					keyFrame = 2;
 			}
 
 			// Fade-in
 			updateAlpha( true, delta_time );
 
-			isAnimating = keyFrame < 2;
+			complete = keyFrame >= 2;
 
-			return new PaintState( 1, 1, cos( expandRatio ), alpha );
+			return new PaintState( 1, 1, cos( bumpRatio ), alpha );
 		}
 
 		@Override
 		public boolean isComplete()
 		{
-			return !isAnimating;
+			return complete;
 		}
 	}
 
-	// ratio to which the image is expanded (0 = hidden, 1 = fully expanded)
-	private double expandRatio = 0;
 
-	private class DeactivateExpand implements Animation
+
+	// =======================================================================
+
+	private class HideExpandButton implements Animation
 	{
+		// TODO (?)
+		private final double expandAnimationSpeed = animationSpeed / imgw;
+
+		// ratio to which the image is expanded (0 = hidden, 1 = fully expanded)
+		private double expandRatio;
+
+		private boolean complete = false;
+
+		public HideExpandButton( Animation currentAnimation )
+		{
+			if ( currentAnimation != null && !currentAnimation.isComplete() && currentAnimation instanceof ShowExpandButton )
+				// if an incomplete ShowExpandButton animation is running, initialize expandRatio to match
+				expandRatio = ( ( ShowExpandButton ) currentAnimation ).expandRatio;
+			else
+				// otherwise start from fully expanded image
+				expandRatio = 1;
+		}
 
 		@Override
 		public PaintState animate( final long delta_time )
 		{
-			// TODO : should become new animation speed
-			final double expandAnimationSpeed = animationSpeed / imgw;
 
 			// Speed up animation by factor 2
 			expandRatio -= 2 * expandAnimationSpeed * delta_time;
@@ -251,21 +328,7 @@ public class SplitPaneOneTouchExpandAnimator implements OverlayAnimator, MouseMo
 			// Fade-out
 			updateAlpha( false, delta_time );
 
-			// Set keyFrame-frames back based on expandRatio
-			if ( expandRatio > 0.5 )
-			{
-				keyFrame = 2;
-			}
-			else if ( expandRatio > 0 )
-			{
-				keyFrame = 1;
-			}
-			else
-			{
-				keyFrame = 0;
-			}
-
-			isAnimating = expandRatio > 0;
+			complete = expandRatio <= 0;
 
 			final double imgRatio = cos( expandRatio );
 			return new PaintState( imgRatio, imgRatio, 0, alpha );
@@ -274,18 +337,44 @@ public class SplitPaneOneTouchExpandAnimator implements OverlayAnimator, MouseMo
 		@Override
 		public boolean isComplete()
 		{
-			return !isAnimating;
+			return complete;
 		}
 	}
 
-	private class ActivateExpand implements Animation
+	private class ShowExpandButton implements Animation
 	{
+		// TODO (?)
+		private final double expandAnimationSpeed = animationSpeed / imgw;
+
+		private int keyFrame;
+
+		// ratio to which the image is expanded (0 = hidden, 1 = fully expanded)
+		private double expandRatio;
+
+		private boolean complete = false;
+
+		public ShowExpandButton( Animation currentAnimation )
+		{
+			if ( currentAnimation != null && !currentAnimation.isComplete() && currentAnimation instanceof HideExpandButton )
+				// if an incomplete HideExpandButton animation is running, initialize expandRatio to match
+				expandRatio = ( ( HideExpandButton ) currentAnimation ).expandRatio;
+			else
+				// otherwise start from fully hidden image
+				expandRatio = 0;
+
+			// initialize keyFrame based on expandRatio
+//			if ( expandRatio > 0.5 )
+//				keyFrame = 2;
+//			else
+			if ( expandRatio > 0 )
+				keyFrame = 1;
+			else
+				keyFrame = 0;
+		}
+
 		@Override
 		public PaintState animate( final long delta_time )
 		{
-			// TODO : should become new animation speed
-			final double expandAnimationSpeed = animationSpeed / imgw;
-
 			// Slide image in
 			if ( keyFrame == 0 )
 			{
@@ -325,7 +414,7 @@ public class SplitPaneOneTouchExpandAnimator implements OverlayAnimator, MouseMo
 			// Fade-in
 			updateAlpha( true, delta_time );
 
-			isAnimating = keyFrame != 3;
+			complete = keyFrame >= 3;
 
 			final double imgRatio = cos( expandRatio );
 			final double bgRatio = keyFrame > 0 ? 1 : imgRatio; // Background should only move out and stay
@@ -335,64 +424,26 @@ public class SplitPaneOneTouchExpandAnimator implements OverlayAnimator, MouseMo
 		@Override
 		public boolean isComplete()
 		{
-			return !isAnimating;
+			return complete;
 		}
 	}
 
-	/**
-	 * Cosine shape acceleration/ deceleration curve  of linear [0,1]
-	 */
-	private double cos( final double t )
-	{
-		return 0.5 - 0.5 * Math.cos( Math.PI * t );
-	}
 
-	private void updateAlpha( final boolean fadeIn, final long delta_time )
-	{
-		if ( fadeIn )
-		{
-			alpha += ( 1 / 250.0 ) * delta_time;
-			alpha = Math.min( 1, alpha );
-		}
-		else
-		{
-			alpha -= ( 1 / 250.0 ) * delta_time;
-			alpha = Math.max( 0.25f, alpha );
-		}
-	}
 
-	private void drawBackground( final Graphics2D g, final int x, final float alpha )
-	{
-		final int y = ( viewPortHeight - imgh ) / 2;
-		final int width = imgw + 60;
-		final int height = imgh;
 
-		g.setColor( new Color( 0, 0, 0, Math.min( alpha, backgroundAlpha ) ) );
-		g.fillRoundRect( x, y, width, height, 25, 25 );
-	}
 
-	private void drawImg( final Graphics2D g, final BufferedImage img, final int x, final float alpha )
-	{
-		final int y = ( viewPortHeight - imgh ) / 2;
 
-		Composite oldComposite = g.getComposite();
-		final AlphaComposite alcom = AlphaComposite.getInstance( AlphaComposite.SRC_OVER, alpha );
-		g.setComposite( alcom );
-		g.drawImage( img, x, y, null );
-		g.setComposite(oldComposite);
-	}
 
-	@Override
-	public boolean isComplete()
-	{
-		return false;
-	}
 
-	@Override
-	public boolean requiresRepaint()
-	{
-		return isAnimating;
-	}
+
+
+
+
+
+
+
+
+	// == MOUSE HANDLER ======================================================
 
 	@Override
 	public void mouseDragged( final MouseEvent e )
@@ -461,16 +512,16 @@ public class SplitPaneOneTouchExpandAnimator implements OverlayAnimator, MouseMo
 		if ( collapsed )
 		{
 			if ( isNearBorder() )
-				repaint( ACTIVATE_EXPAND );
+				repaint( SHOW_EXPAND );
 			else
-				repaint( DEACTIVATE_EXPAND );
+				repaint( HIDE_EXPAND );
 		}
 		else
 		{
 			if ( isInTriggerRegion() )
-				repaint( ACTIVATE_COLLAPS );
+				repaint( SHOW_COLLAPSE );
 			else
-				repaint( DEACTIVATE_COLLAPS );
+				repaint( HIDE_COLLAPSE );
 		}
 	}
 
@@ -511,10 +562,8 @@ public class SplitPaneOneTouchExpandAnimator implements OverlayAnimator, MouseMo
 			splitPanel.collapseUI();
 			if ( collapsed )
 			{
-				expandRatio = 1;
-				keyFrame = 3;
 				isAnimating = false;
-				repaint( DEACTIVATE_EXPAND );
+				repaint( HIDE_EXPAND );
 			}
 			splitPanel.getViewerPanel().requestRepaint();
 		}
