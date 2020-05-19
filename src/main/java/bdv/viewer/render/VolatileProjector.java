737ef23a401224b645fa25e0f5f8a89843d285fb
tpietzsch <tobias.pietzsch@gmail.com>
2020-05-19 23:22:15 +0200

    Clean up VolatileProjector interface

--- src/main/java/bdv/viewer/render/VolatileProjector.java
+++ src/main/java/bdv/viewer/render/VolatileProjector.java
@@ -41,10 +41,10 @@ public interface VolatileProjector extends InterruptibleProjector
 	 * @return true if rendering was completed (all target pixels written).
 	 *         false if rendering was interrupted.
 	 */
-	public boolean map( boolean clearUntouchedTargetPixels );
+	boolean map( boolean clearUntouchedTargetPixels );
 
 	/**
 	 * @return true if all mapped pixels were {@link Volatile#isValid() valid}.
 	 */
-	public boolean isValid();
+	boolean isValid();
 }
