c73c62d2a033998d4e25e3364cad6337f10acced
tpietzsch <tobias.pietzsch@gmail.com>
2020-05-27 13:06:17 +0200

    WIP: Transform notification logic
    ViewerPanel doesn't implement TransformListener

--- src/main/java/bdv/viewer/ViewerPanel.java
+++ src/main/java/bdv/viewer/ViewerPanel.java
@@ -97,7 +97,7 @@ import net.imglib2.util.LinAlgHelpers;
  *
  * @author Tobias Pietzsch
  */
-public class ViewerPanel extends JPanel implements OverlayRenderer, TransformListener< AffineTransform3D >, PainterThread.Paintable, ViewerStateChangeListener, RequestRepaint
+public class ViewerPanel extends JPanel implements OverlayRenderer, PainterThread.Paintable, ViewerStateChangeListener, RequestRepaint
 {
 	private static final long serialVersionUID = 1L;
 
@@ -251,7 +251,7 @@ public class ViewerPanel extends JPanel implements OverlayRenderer, TransformLis
 		threadGroup = new ThreadGroup( this.toString() );
 		painterThread = new PainterThread( threadGroup, this );
 		painterThread.setDaemon( true );
-		transformEventHandler = options.getTransformEventHandlerFactory().create( this );
+		transformEventHandler = options.getTransformEventHandlerFactory().create( null /*TODO*/ );
 		transformEventHandler.setTransformStore( state()::getViewerTransform, state()::setViewerTransform );
 		renderTarget = new BufferedImageOverlayRenderer();
 		display = new InteractiveDisplayCanvas( options.getWidth(), options.getHeight() );
@@ -544,12 +544,6 @@ public class ViewerPanel extends JPanel implements OverlayRenderer, TransformLis
 
 		if ( requiresRepaint )
 			display.repaint();
-	}
-
-	@Override
-	public void transformChanged( final AffineTransform3D transform )
-	{
-		throw new UnsupportedOperationException();
 	}
 
 	@Override
