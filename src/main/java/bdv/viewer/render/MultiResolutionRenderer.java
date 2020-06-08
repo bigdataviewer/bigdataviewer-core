3f08c9dd7a88c3d6cc74ba66dceecd1ecd044051
tpietzsch <tobias.pietzsch@gmail.com>
2020-06-08 20:18:43 +0200

    WIP: Refactor projector creation to separate class ProjectorFactory
    MultiResolutionRenderer: update javadoc

--- src/main/java/bdv/viewer/render/MultiResolutionRenderer.java
+++ src/main/java/bdv/viewer/render/MultiResolutionRenderer.java
@@ -46,60 +46,47 @@ import net.imglib2.ui.overlay.BufferedImageOverlayRenderer;
 import net.imglib2.util.Fraction;
 
 /**
- * A renderer that uses a coarse-to-fine rendering scheme. First, a
- * small target image at a fraction of the canvas resolution is
- * rendered. Then, increasingly larger images are rendered, until the full
- * canvas resolution is reached.
+ * A renderer that uses a coarse-to-fine rendering scheme. First, a small target
+ * image at a fraction of the canvas resolution is rendered. Then, increasingly
+ * larger images are rendered, until the full canvas resolution is reached.
  * <p>
- * When drawing the low-resolution target images to the screen, they
- * will be scaled up by Java2D (or JavaFX, etc) to the full canvas size, which is relatively
- * fast. Rendering the small, low-resolution images is usually very fast, such
- * that the display is very interactive while the user changes the viewing
- * transformation for example. When the transformation remains fixed for a
- * longer period, higher-resolution details are filled in successively.
+ * When drawing the low-resolution target images to the screen, they will be
+ * scaled up by Java2D (or JavaFX, etc) to the full canvas size, which is
+ * relatively fast. Rendering the small, low-resolution images is usually very
+ * fast, such that the display is very interactive while the user changes the
+ * viewing transformation for example. When the transformation remains fixed for
+ * a longer period, higher-resolution details are filled in successively.
  * <p>
- * The renderer allocates a {@code BufferedImage} for each of a predefined set
- * of <em>screen scales</em> (a screen scale of 1 means that 1 pixel in the
- * screen image is displayed as 1 pixel on the canvas, a screen scale of 0.5
- * means 1 pixel in the screen image is displayed as 2 pixel on the canvas,
- * etc.)
+ * The renderer allocates a {@code RenderResult} for each of a predefined set of
+ * <em>screen scales</em> (a screen scale of 1 means that 1 pixel in the screen
+ * image is displayed as 1 pixel on the canvas, a screen scale of 0.5 means 1
+ * pixel in the screen image is displayed as 2 pixel on the canvas, etc.)
  * <p>
- * At any time, one of these screen scales is selected as the
- * <em>highest screen scale</em>. Rendering starts with this highest screen
- * scale and then proceeds to lower screen scales (higher resolution images).
- * Unless the highest screen scale is currently rendering,
- * {@link #requestRepaint() repaint request} will cancel rendering, such that
- * display remains interactive.
+ * At any time, one of these screen scales is selected as the <em>highest screen
+ * scale</em>. Rendering starts with this highest screen scale and then proceeds
+ * to lower screen scales (higher resolution images). Unless the highest screen
+ * scale is currently rendering, {@link #requestRepaint() repaint request} will
+ * cancel rendering, such that display remains interactive.
  * <p>
  * The renderer tries to maintain a per-frame rendering time close to a desired
- * number of <code>targetRenderNanos</code> nanoseconds. If the rendering time
- * (in nanoseconds) for the (currently) highest scaled screen image is above
- * this threshold, a coarser screen scale is chosen as the highest screen scale
- * to use. Similarly, if the rendering time for the (currently) second-highest
+ * number of {@code targetRenderNanos} nanoseconds. If the rendering time (in
+ * nanoseconds) for the (currently) highest scaled screen image is above this
+ * threshold, a coarser screen scale is chosen as the highest screen scale to
+ * use. Similarly, if the rendering time for the (currently) second-highest
  * scaled screen image is below this threshold, this finer screen scale chosen
  * as the highest screen scale to use.
  * <p>
- * The renderer uses multiple threads (if desired) and double-buffering (if
- * desired).
- * <p>
- * Double buffering means that three {@code BufferedImage BufferedImages} are
- * created for every screen scale. After rendering the first one of them and
- * setting it to the {@link RenderTarget}, next time, rendering goes to the
- * second one, then to the third. The {@link RenderTarget} will always have a
- * complete image, which is not rendered to while it is potentially drawn to the
- * screen. When setting an image to the {@link RenderTarget}, the
- * {@link RenderTarget} will release one of the previously set images to be
- * rendered again. Thus, rendering will not interfere with painting the
- * {@code BufferedImage} to the canvas.
+ * The renderer uses multiple threads (if desired).
  * <p>
  * The renderer supports rendering of {@link Volatile} sources. In each
  * rendering pass, all currently valid data for the best fitting mipmap level
- * and all coarser levels is rendered to a {@link #renderImages temporary image}
- * for each visible source. Then the temporary images are combined to the final
- * image for display. The number of passes required until all data is valid
- * might differ between visible sources.
+ * and all coarser levels is rendered to a temporary image for each visible
+ * source. Then the temporary images are combined to the final image for
+ * display. The number of passes required until all data is valid might differ
+ * between visible sources.
  * <p>
- * Rendering timing is tied to a {@link CacheControl} control for IO budgeting, etc.
+ * Rendering timing is tied to a {@link CacheControl} control for IO budgeting,
+ * etc.
  *
  * @author Tobias Pietzsch
  */
@@ -152,7 +139,8 @@ public class MultiResolutionRenderer
 	private VolatileProjector projector;
 
 	/**
-	 * The index of the screen scale of the {@link #projector current projector}.
+	 * The index of the screen scale of the {@link #projector current
+	 * projector}.
 	 */
 	private int currentScreenScaleIndex;
 
@@ -164,8 +152,8 @@ public class MultiResolutionRenderer
 	private RenderImage[][] renderImages;
 
 	/**
-	 * Storage for mask images of {@link VolatileHierarchyProjector}.
-	 * One array per visible source. (First) index is index in list of visible sources.
+	 * Storage for mask images of {@link VolatileHierarchyProjector}. One array
+	 * per visible source. (First) index is index in list of visible sources.
 	 */
 	private byte[][] renderMaskArrays;
 
@@ -184,11 +172,10 @@ public class MultiResolutionRenderer
 	private final double[] screenScales;
 
 	/**
-	 * The scale transformation from viewer to screen image.
-	 * Each transformations corresponds to a {@link #screenScales screen
-	 * scale}.
+	 * The scale transformation from viewer to screen image. Each
+	 * transformations corresponds to a {@link #screenScales screen scale}.
 	 */
-	private AffineTransform3D[] screenScaleTransforms;
+	private final AffineTransform3D[] screenScaleTransforms;
 
 	/**
 	 * If the rendering time (in nanoseconds) for the (currently) highest scaled
@@ -216,12 +203,16 @@ public class MultiResolutionRenderer
 	private int requestedScreenScaleIndex;
 
 	/**
-	 * Whether the current rendering operation may be cancelled (to start a
-	 * new one). Rendering may be cancelled unless we are rendering at
-	 * coarsest screen scale and coarsest mipmap level.
+	 * Whether the current rendering operation may be cancelled (to start a new
+	 * one). Rendering may be cancelled unless we are rendering at coarsest
+	 * screen scale and coarsest mipmap level.
 	 */
 	private volatile boolean renderingMayBeCancelled;
 
+	/**
+	 * Creates projectors for rendering current {@code ViewerState} to a
+	 * {@code screenImage}.
+	 */
 	private final ProjectorFactory projectorFactory;
 
 	/**
@@ -236,7 +227,7 @@ public class MultiResolutionRenderer
 	private boolean newFrameRequest;
 
 	// TODO: should be settable
-	private long[] iobudget = new long[] { 100l * 1000000l,  10l * 1000000l };
+	private final long[] iobudget = new long[] { 100l * 1000000l,  10l * 1000000l };
 
 	/**
 	 * @param display
