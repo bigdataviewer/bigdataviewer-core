5c8f891b007964a87daabb38bd22035b4ec1015e
tpietzsch <tobias.pietzsch@gmail.com>
2021-01-19 22:57:01 +0100

    WIP laf switcher
    Don't pack frames when LAF is updated (destroys window size)

--- src/main/java/bdv/BigDataViewer.java
+++ src/main/java/bdv/BigDataViewer.java
@@ -29,8 +29,13 @@
 package bdv;
 
 import bdv.tools.PreferencesDialog;
+import bdv.LafSelection.LafDialog;
 import bdv.viewer.ConverterSetups;
 import bdv.viewer.ViewerState;
+import com.formdev.flatlaf.FlatDarculaLaf;
+import com.formdev.flatlaf.FlatDarkLaf;
+import com.formdev.flatlaf.FlatIntelliJLaf;
+import com.formdev.flatlaf.FlatLightLaf;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
@@ -762,7 +767,18 @@ public class BigDataViewer
 		{
 			System.setProperty( "apple.laf.useScreenMenuBar", "true" );
 
+			FlatLightLaf.installLafInfo();
+			FlatDarkLaf.installLafInfo();
+			FlatDarculaLaf.installLafInfo();
+			FlatIntelliJLaf.installLafInfo();
+//			FlatDarculaLaf.install();
+//			FlatIntelliJLaf.install();
+
 			final BigDataViewer bdv = open( fn, new File( fn ).getName(), new ProgressWriterConsole(), ViewerOptions.options() );
+
+			final LafDialog lafDialog = new LafDialog();
+			lafDialog.addComponent( bdv.getViewerFrame() );
+			lafDialog.setVisible( true );
 
 //			DumpInputConfig.writeToYaml( System.getProperty( "user.home" ) + "/.bdv/bdvkeyconfig.yaml", bdv.getViewerFrame() );
 		}
