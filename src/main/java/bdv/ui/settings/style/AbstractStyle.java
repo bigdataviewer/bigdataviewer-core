6639eaa47f28f54d1c7bc0758f5f75711ca97e35
tpietzsch <tobias.pietzsch@gmail.com>
2021-03-07 22:37:56 +0100

    Copy SettingsPanel etc from Mastodon

---
+++ src/main/java/bdv/ui/settings/style/AbstractStyle.java
@@ -0,0 +1,25 @@
+package bdv.ui.settings.style;
+
+// TODO move to package org.mastodon.app.ui.settings.style
+// TODO (Is this a bit overkill?)
+public abstract class AbstractStyle< S extends AbstractStyle< S > > implements Style< S>
+{
+	private String name;
+
+	protected AbstractStyle( final String name )
+	{
+		this.name = name;
+	}
+
+	@Override
+	public String getName()
+	{
+		return name;
+	}
+
+	@Override
+	public void setName( final String name )
+	{
+		this.name = name;
+	}
+}
