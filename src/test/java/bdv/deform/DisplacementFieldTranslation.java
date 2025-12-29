59cc2c51bb04d52544522581abc83042225350a7
tpietzsch <tobias.pietzsch@gmail.com>
2025-12-29 17:13:28 +0100

    debugging translation

--- src/test/java/bdv/deform/DisplacementFieldTranslation.java
+++ src/test/java/bdv/deform/DisplacementFieldTranslation.java
@@ -10,6 +10,7 @@ import bdv.util.BdvFunctions;
 import bdv.util.BdvSource;
 import ij.IJ;
 import ij.ImagePlus;
+import net.imglib2.algorithm.blocks.BlockAlgoUtils;
 import net.imglib2.algorithm.blocks.BlockSupplier;
 import net.imglib2.algorithm.blocks.dfield.DisplacementField;
 import net.imglib2.algorithm.blocks.transform.Transform;
@@ -33,36 +34,56 @@ public class DisplacementFieldTranslation
 
 		BdvSource bdv = BdvFunctions.show( img, "original image", Bdv.options().is2D() );
 
-		final double[] dfieldData = {
-				-.5, -.5,
-				.5, -.5,
-				-.5, .5,
-				.5, .5
+		final double[][] dfieldDatas = {
+//				{
+//						-.5, -.5,
+//						0, 0,
+//						0, 0,
+//						0, 0,
+//				},
+//				{
+//						0, 0,
+//						.5, -.5,
+//						0, 0,
+//						0, 0,
+//				},
+//				{
+//						0, 0,
+//						0, 0,
+//						-.5, .5,
+//						0, 0,
+//				},
+//				{
+//						0, 0,
+//						0, 0,
+//						0, 0,
+//						.5, .5
+//				},
+				{
+						-.5, -.5,
+						.5, -.5,
+						-.5, .5,
+						.5, .5
+				},
 		};
 
-//				0,0,
-//				0,0,
+		for ( int i = 0; i < dfieldDatas.length; i++ )
+		{
+			final AffineTransform2D transformFromSource = new AffineTransform2D();
+			transformFromSource.scale( 16 );
+			transformFromSource.translate( 7.5, 7.5 );
 
-//		final double[] dfieldData = {
-//				.25,  .25,
-//				-.25,  .25,
-//				.25, -.25,
-//				-.25, -.25
-//		};
-		final Img< DoubleType > dfieldArray = ArrayImgs.doubles( dfieldData, 2, 2, 2 );
-
-		final AffineTransform2D transformFromSource = new AffineTransform2D();
-		transformFromSource.scale( 16 );
-		final DisplacementField< DoubleType > dfield = new DisplacementField<>(
-				BlockSupplier.of( dfieldArray ),
-				new double[] { 16, 16 },
-				new double[] { 0, 0 } );
-		final BlockSupplier< UnsignedByteType > blocks = BlockSupplier
-				.of( img.view().extend(border()) )
-				.andThen( displacementFieldAffine( transformFromSource, dfield, Transform.Interpolation.NLINEAR ) );
-		final Img< UnsignedByteType > tformedBlocks = blocks.toCellImg( img.dimensionsAsLongArray(), 8, 8 );
-		BdvFunctions.show( tformedBlocks, "transformed image (displacementFieldAffine)", Bdv.options().addTo( bdv ) );
-
-
+			final Img< DoubleType > dfieldArray = ArrayImgs.doubles( dfieldDatas[ i ], 2, 2, 2 );
+			final DisplacementField< DoubleType > dfield = new DisplacementField<>(
+					BlockSupplier.of( dfieldArray ),
+					new double[] { 16, 16 },
+					new double[] { 7.5, 7.5 } );
+			final BlockSupplier< UnsignedByteType > blocks = BlockSupplier
+					.of( img.view().extend( border() ) )
+					.andThen( displacementFieldAffine( transformFromSource, dfield, Transform.Interpolation.NLINEAR ) );
+//			final Img< UnsignedByteType > tformedBlocks = blocks.toCellImg( img.dimensionsAsLongArray(), 8, 8 );
+			final Img< UnsignedByteType > tformedBlocks = BlockAlgoUtils.arrayImg( blocks, img );
+			BdvFunctions.show( tformedBlocks, "transformed (" + i + ")", Bdv.options().addTo( bdv ) );
+		}
 	}
 }
